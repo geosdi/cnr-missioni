19b281b1115a8057cb127211c69469e0cc037782
Giuseppe La Scaleia <glascaleia@gmail.com>
2015-12-17 09:36:26 +0100

    Some Improvements. FIXED Tests

--- missioni-core/missioni-dao/src/test/java/it/cnr/missioni/el/dao/MissioneDAOTest.java
+++ missioni-core/missioni-dao/src/test/java/it/cnr/missioni/el/dao/MissioneDAOTest.java
@@ -1,11 +1,11 @@
 package it.cnr.missioni.el.dao;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.annotation.Resource;
-
+import com.google.common.collect.Lists;
+import it.cnr.missioni.model.missione.Missione;
+import it.cnr.missioni.model.missione.StatoEnum;
+import org.elasticsearch.action.bulk.BulkResponse;
 import org.geosdi.geoplatform.experimental.el.configurator.GPIndexConfigurator;
+import org.geosdi.geoplatform.experimental.el.dao.GPElasticSearchDAO;
 import org.geosdi.geoplatform.experimental.el.index.GPIndexCreator;
 import org.geosdi.geoplatform.logger.support.annotation.GeoPlatformLog;
 import org.junit.Assert;
@@ -18,117 +18,149 @@ import org.slf4j.Logger;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import it.cnr.missioni.model.missione.Missione;
-import it.cnr.missioni.model.missione.StatoEnum;
-import it.cnr.missioni.model.utente.Anagrafica;
-import it.cnr.missioni.model.utente.Utente;
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 /**
  * @author Salvia Vito
  */
 @FixMethodOrder(value = MethodSorters.NAME_ASCENDING)
 @RunWith(value = SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath:applicationContext-DAO-Test.xml" })
+@ContextConfiguration(locations = {"classpath:applicationContext-DAO-Test.xml"})
 public class MissioneDAOTest {
 
-	@GeoPlatformLog
-	static Logger logger;
-	@Resource(name = "missioniIndexConfigurator")
-	private GPIndexConfigurator missioniDocIndexConfigurator;
-	@Resource(name = "missioneIndexCreator")
-	private GPIndexCreator missioneDocIndexCreator;
+    @GeoPlatformLog
+    static Logger logger;
+    @Resource(name = "missioniIndexConfigurator")
+    private GPIndexConfigurator missioniDocIndexConfigurator;
+    @Resource(name = "missioneIndexCreator")
+    private GPIndexCreator missioneDocIndexCreator;
 
-	@Resource(name = "missioneDAO")
-	private IMissioneDAO missioneDAO;
+    @Resource(name = "missioneDAO")
+    private IMissioneDAO missioneDAO;
 
-	private List<Missione> listaMissioni = new ArrayList<Missione>();
+    private List<Missione> listaMissioni = new ArrayList<Missione>();
 
-	@Before
-	public void setUp() {
-		Assert.assertNotNull(missioneDocIndexCreator);
-		Assert.assertNotNull(missioniDocIndexConfigurator);
-		Assert.assertNotNull(missioneDAO);
-	}
+    @Before
+    public void setUp() {
+        Assert.assertNotNull(missioneDocIndexCreator);
+        Assert.assertNotNull(missioniDocIndexConfigurator);
+        Assert.assertNotNull(missioneDAO);
+    }
 
-	@Test
-	public void A_createMissioneCNRTest() throws Exception {
+    @Test
+    public void A_createMissioneCNRTest() throws Exception {
+        creaMissioni();
+        missioneDAO.persist(this.listaMissioni);
+        Thread.sleep(1000);
+        logger.debug("############################NUMBER_OF_MISSIONI : {}\n", this.missioneDAO.count().intValue());
 
-		creaMissioni();
-		missioneDAO.persist(this.listaMissioni);
-		Thread.sleep(1000);
-		logger.debug("############################NUMBER_OF_MISSIONI : {}\n", this.missioneDAO.count().intValue());
+    }
 
-	}
+    @Test
+    public void B_findMissioneByUtente_1Test() throws Exception {
+        List<Missione> lista = this.missioneDAO.findMissioneByUtenteStato("01", null);
+        Assert.assertTrue("FIND MISSIONE BY UTENTE", lista.size() == 2);
 
-	@Test
-	public void B_findMissioneByUtente_1Test() throws Exception {
-		List<Missione> lista = this.missioneDAO.findMissioneByUtenteStato("01",null);
-		Assert.assertTrue("FIND MISSIONE BY UTENTE", lista.size() == 2);
+    }
 
-	}
-	
-	@Test
-	public void C_findMissioneByUtente_2Test() throws Exception {
-		List<Missione> lista = this.missioneDAO.findMissioneByUtenteStato("03",null);
-		Assert.assertTrue("FIND MISSIONE BY UTENTE", lista.isEmpty());
+    @Test
+    public void C_findMissioneByUtente_2Test() throws Exception {
+        List<Missione> lista = this.missioneDAO.findMissioneByUtenteStato("03", null);
+        Assert.assertTrue("FIND MISSIONE BY UTENTE", lista.isEmpty());
 
-	}
-	
-	@Test
-	public void D_findMissioneByStato_1Test() throws Exception {
-		List<Missione> lista = this.missioneDAO.findMissioneByUtenteStato(null,StatoEnum.PRESA_IN_CARICO);
-		Assert.assertTrue("FIND MISSIONE BY STATO", lista.size() == 2);
+    }
 
-	}
-	
-	@Test
-	public void E_findMissioneByStato_2Test() throws Exception {
-		List<Missione> lista = this.missioneDAO.findMissioneByUtenteStato(null,StatoEnum.APPROVATA);
-		Assert.assertTrue("FIND MISSIONE BY STATO", lista.size() == 1);
+    @Test
+    public void D_findMissioneByStato_1Test() throws Exception {
+        List<Missione> lista = this.missioneDAO.findMissioneByUtenteStato(null, StatoEnum.PRESA_IN_CARICO);
+        Assert.assertTrue("FIND MISSIONE BY STATO", lista.size() == 2);
 
-	}
-	
-	@Test
-	public void E_findMissioneByUtenteStato_1Test() throws Exception {
-		List<Missione> lista = this.missioneDAO.findMissioneByUtenteStato("01",StatoEnum.APPROVATA);
-		Assert.assertTrue("FIND MISSIONE BY STATO", lista.isEmpty());
+    }
 
-	}
-	
-	@Test
-	public void E_findMissioneByUtenteStato_2Test() throws Exception {
-		List<Missione> lista = this.missioneDAO.findMissioneByUtenteStato("01",StatoEnum.PRESA_IN_CARICO);
-		Assert.assertTrue("FIND MISSIONE BY STATO", lista.size() == 2);
+    @Test
+    public void E_findMissioneByStato_2Test() throws Exception {
+        List<Missione> lista = this.missioneDAO.findMissioneByUtenteStato(null, StatoEnum.APPROVATA);
+        Assert.assertTrue("FIND MISSIONE BY STATO", lista.size() == 1);
 
-	}
+    }
+
+    @Test
+    public void E_findMissioneByUtenteStato_1Test() throws Exception {
+        List<Missione> lista = this.missioneDAO.findMissioneByUtenteStato("01", StatoEnum.APPROVATA);
+        Assert.assertTrue("FIND MISSIONE BY STATO", lista.isEmpty());
+
+    }
+
+    @Test
+    public void E_findMissioneByUtenteStato_2Test() throws Exception {
+        List<Missione> lista = this.missioneDAO.findMissioneByUtenteStato("01", StatoEnum.PRESA_IN_CARICO);
+        Assert.assertTrue("FIND MISSIONE BY STATO", lista.size() == 2);
+    }
+
+    @Test
+    public void F_insertMassiveMissioniTest() throws Exception {
+        BulkResponse bulkResponse = this.missioneDAO.persist(createMassiveMissioni());
+        logger.info("#####################MASSIVE_MISSIONI_INSERT_TIME : {}\n",
+                bulkResponse.getTook().toString());
+    }
+
+    @Test
+    public void G_findMissioniWithPageTest() throws Exception {
+        GPElasticSearchDAO.IPageResult<Missione> pageResult = this.missioneDAO
+                .find(new GPElasticSearchDAO.Page(0, 3000));
+        logger.info("########################MISSIONI_FOUND : {}\n", pageResult.getTotal());
+    }
+
+    @Test
+    public void tearDown() throws Exception {
+        this.missioneDocIndexCreator.deleteIndex();
+    }
 
 
-	private void creaMissioni() {
-		Missione missione = null;
-		missione = new Missione();
-		missione.setId("M_01");
-		missione.setOggetto("Conferenza");
-		missione.setLocalita("Roma");
-		missione.setIdUtente("01");
-		missione.setStato(StatoEnum.PRESA_IN_CARICO);
-		listaMissioni.add(missione);
+    private void creaMissioni() {
+        Missione missione = new Missione();
+        missione.setId("M_01");
+        missione.setOggetto("Conferenza");
+        missione.setLocalita("Roma");
+        missione.setIdUtente("01");
+        missione.setStato(StatoEnum.PRESA_IN_CARICO);
+        listaMissioni.add(missione);
 
-		missione = new Missione();
-		missione.setId("M_02");
-		missione.setOggetto("Conferenza");
-		missione.setLocalita("Milano");
-		missione.setIdUtente("01");
-		missione.setStato(StatoEnum.PRESA_IN_CARICO);
-		listaMissioni.add(missione);
+        missione = new Missione();
+        missione.setId("M_02");
+        missione.setOggetto("Conferenza");
+        missione.setLocalita("Milano");
+        missione.setIdUtente("01");
+        missione.setStato(StatoEnum.PRESA_IN_CARICO);
+        listaMissioni.add(missione);
 
-		missione = new Missione();
-		missione.setId("M_03");
-		missione.setOggetto("Riunione");
-		missione.setLocalita("Milano");
-		missione.setIdUtente("02");
-		missione.setStato(StatoEnum.APPROVATA);
-		listaMissioni.add(missione);
+        missione = new Missione();
+        missione.setId("M_03");
+        missione.setOggetto("Riunione");
+        missione.setLocalita("Milano");
+        missione.setIdUtente("02");
+        missione.setStato(StatoEnum.APPROVATA);
+        listaMissioni.add(missione);
+    }
 
-	}
-
+    List<Missione> createMassiveMissioni() {
+        List<Missione> missioni = Lists.newArrayList();
+        for (int i = 0; i < 8000; i++) {
+            Missione missione = new Missione();
+            missione.setId(UUID.randomUUID().toString());
+            missione.setOggetto("OGGETTO-TEST - " + i);
+            missione.setLocalita("LOCALITAA' - " + i);
+            missione.setIdUtente(UUID.randomUUID().toString());
+            if ((i % 2) == 0) {
+                missione.setStato(StatoEnum.APPROVATA);
+            } else {
+                missione.setStato(StatoEnum.PRESA_IN_CARICO);
+            }
+            missioni.add(missione);
+        }
+        return missioni;
+    }
 }
--- missioni-core/missioni-dao/src/test/java/it/cnr/missioni/el/dao/UtenteDAOTest.java
+++ missioni-core/missioni-dao/src/test/java/it/cnr/missioni/el/dao/UtenteDAOTest.java
@@ -1,10 +1,8 @@
 package it.cnr.missioni.el.dao;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.annotation.Resource;
-
+import it.cnr.missioni.model.utente.Anagrafica;
+import it.cnr.missioni.model.utente.Credenziali;
+import it.cnr.missioni.model.utente.Utente;
 import org.geosdi.geoplatform.experimental.el.configurator.GPIndexConfigurator;
 import org.geosdi.geoplatform.experimental.el.index.GPIndexCreator;
 import org.geosdi.geoplatform.logger.support.annotation.GeoPlatformLog;
@@ -18,121 +16,118 @@ import org.slf4j.Logger;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import it.cnr.missioni.model.utente.Anagrafica;
-import it.cnr.missioni.model.utente.Credenziali;
-import it.cnr.missioni.model.utente.Utente;
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Salvia Vito
  */
 @FixMethodOrder(value = MethodSorters.NAME_ASCENDING)
 @RunWith(value = SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath:applicationContext-DAO-Test.xml" })
+@ContextConfiguration(locations = {"classpath:applicationContext-DAO-Test.xml"})
 public class UtenteDAOTest {
 
-	@GeoPlatformLog
-	static Logger logger;
-	@Resource(name = "missioniIndexConfigurator")
-	private GPIndexConfigurator missioniDocIndexConfigurator;
+    @GeoPlatformLog
+    static Logger logger;
+    @Resource(name = "missioniIndexConfigurator")
+    private GPIndexConfigurator missioniDocIndexConfigurator;
 
-	@Resource(name = "utenteIndexCreator")
-	private GPIndexCreator utenteDocIndexCreator;
+    @Resource(name = "utenteIndexCreator")
+    private GPIndexCreator utenteDocIndexCreator;
 
-	@Resource(name = "utenteDAO")
-	private IUtenteDAO utenteDAO;
+    @Resource(name = "utenteDAO")
+    private IUtenteDAO utenteDAO;
 
-	private List<Utente> listaUtenti = new ArrayList<Utente>();
+    private List<Utente> listaUtenti = new ArrayList<Utente>();
 
-	@Before
-	public void setUp() {
-		Assert.assertNotNull(utenteDocIndexCreator);
-		Assert.assertNotNull(missioniDocIndexConfigurator);
-		Assert.assertNotNull(utenteDAO);
-	}
+    @Before
+    public void setUp() {
+        Assert.assertNotNull(utenteDocIndexCreator);
+        Assert.assertNotNull(missioniDocIndexConfigurator);
+        Assert.assertNotNull(utenteDAO);
+    }
 
-	@Test
-	public void A_createUtenteCNRTest() throws Exception {
-		creaUtenti();
-		utenteDAO.persist(listaUtenti);
-		Thread.sleep(1000);
-		logger.debug("############################NUMBER_OF_UTENTI : {}\n", this.utenteDAO.count().intValue());
-	}
+    @Test
+    public void A_createUtenteCNRTest() throws Exception {
+        creaUtenti();
+        utenteDAO.persist(listaUtenti);
+        Thread.sleep(1000);
+        logger.debug("############################NUMBER_OF_UTENTI : {}\n", this.utenteDAO.count().intValue());
+    }
 
-	@Test
-	public void B_findUtenteByUsernameValidaTest() throws Exception {
-		Utente utente = utenteDAO.findUtenteByUsername("vito.salvia");
-		logger.debug("############################UTENTE_WITH_USERNAME : {}\n",
-				utente.getAnagrafica().getCognome() + " " + utente.getAnagrafica().getNome());
-		Assert.assertTrue("FIND UTENTE BY USERNAME VALIDA", utente != null);
-	}
-	
-	@Test
-	public void C_findUtenteByUsernameErrataTest() throws Exception {
-		Utente utente = utenteDAO.findUtenteByUsername("vito.salvi");
-		Assert.assertTrue("FIND UTENTE BY USERNAME ERRATA", utente == null);
-	}
+    @Test
+    public void B_findUtenteByUsernameValidaTest() throws Exception {
+        Utente utente = utenteDAO.findUtenteByUsername("vito.salvia");
+        logger.debug("############################UTENTE_WITH_USERNAME : {}\n",
+                utente.getAnagrafica().getCognome() + " " + utente.getAnagrafica().getNome());
+        Assert.assertTrue("FIND UTENTE BY USERNAME VALIDA", utente != null);
+    }
 
-	@Test
-	public void D_updatePasswordUtente() throws Exception {
-		Utente utente = utenteDAO.findUtenteByUsername("vito.salvia");
-		String oldPassword = utente.getCredenziali().getPassword();
-		utente.getCredenziali().setPassword(utente.getCredenziali().md5hash("salvia.vito"));
-		utenteDAO.persist(utente);
-		Thread.sleep(1000);
-		utente = utenteDAO.findUtenteByUsername("vito.salvia");
-		String newPassword = utente.getCredenziali().getPassword();
-		Assert.assertTrue("Update Password Utente", !oldPassword.equals(newPassword));
+    @Test
+    public void C_findUtenteByUsernameErrataTest() throws Exception {
+        Utente utente = utenteDAO.findUtenteByUsername("vito.salvi");
+        Assert.assertTrue("FIND UTENTE BY USERNAME ERRATA", utente == null);
+    }
 
-	}
+    @Test
+    public void D_updatePasswordUtente() throws Exception {
+        Utente utente = utenteDAO.findUtenteByUsername("vito.salvia");
+        String oldPassword = utente.getCredenziali().getPassword();
+        utente.getCredenziali().setPassword(utente.getCredenziali().md5hash("salvia.vito"));
+        utenteDAO.persist(utente);
+        Thread.sleep(1000);
+        utente = utenteDAO.findUtenteByUsername("vito.salvia");
+        String newPassword = utente.getCredenziali().getPassword();
+        Assert.assertTrue("Update Password Utente", !oldPassword.equals(newPassword));
 
-	// @Test
-	// public void C_deleteTest() throws Exception {
-	// utenteDAO.removeAll();
-	// logger.debug("############################NUMBER_OF_DOCS_AFTER_DELETING :
-	// {}\n",
-	// this.utenteDAO.count().intValue());
-	// }
+    }
 
-	private void creaUtenti() {
-		Utente utente = null;
-		Anagrafica anagrafica = null;
-		Credenziali credenziali = null;
-		utente = new Utente();
-		utente.setId("01");
-		anagrafica = new Anagrafica();
-		anagrafica.setCognome("Salvia");
-		anagrafica.setNome("Vito");
-		credenziali = new Credenziali();
-		credenziali.setUsername("vito.salvia");
-		credenziali.setPassword(credenziali.md5hash("vitosalvia"));
-		utente.setCredenziali(credenziali);
-		utente.setAnagrafica(anagrafica);
-		listaUtenti.add(utente);
+    @Test
+    public void tearDown() throws Exception {
+        this.utenteDocIndexCreator.deleteIndex();
+    }
 
-		utente = new Utente();
-		utente.setId("02");
-		anagrafica = new Anagrafica();
-		anagrafica.setCognome("Rossi");
-		anagrafica.setNome("Paolo");
-		credenziali = new Credenziali();
-		credenziali.setUsername("paolo.rossi");
-		credenziali.setPassword(credenziali.md5hash("paolorossi"));
-		utente.setCredenziali(credenziali);
-		utente.setAnagrafica(anagrafica);
-		listaUtenti.add(utente);
-		
-		utente = new Utente();
-		utente.setId("03");
-		anagrafica = new Anagrafica();
-		anagrafica.setCognome("Mario");
-		anagrafica.setNome("Bianchi");
-		credenziali = new Credenziali();
-		credenziali.setUsername("mario.bianchi");
-		credenziali.setPassword(credenziali.md5hash("mariobianchi"));
-		utente.setCredenziali(credenziali);
-		utente.setAnagrafica(anagrafica);
-		listaUtenti.add(utente);
-		
-	}
+    private void creaUtenti() {
+        Utente utente = null;
+        Anagrafica anagrafica = null;
+        Credenziali credenziali = null;
+        utente = new Utente();
+        utente.setId("01");
+        anagrafica = new Anagrafica();
+        anagrafica.setCognome("Salvia");
+        anagrafica.setNome("Vito");
+        credenziali = new Credenziali();
+        credenziali.setUsername("vito.salvia");
+        credenziali.setPassword(credenziali.md5hash("vitosalvia"));
+        utente.setCredenziali(credenziali);
+        utente.setAnagrafica(anagrafica);
+        listaUtenti.add(utente);
+
+        utente = new Utente();
+        utente.setId("02");
+        anagrafica = new Anagrafica();
+        anagrafica.setCognome("Rossi");
+        anagrafica.setNome("Paolo");
+        credenziali = new Credenziali();
+        credenziali.setUsername("paolo.rossi");
+        credenziali.setPassword(credenziali.md5hash("paolorossi"));
+        utente.setCredenziali(credenziali);
+        utente.setAnagrafica(anagrafica);
+        listaUtenti.add(utente);
+
+        utente = new Utente();
+        utente.setId("03");
+        anagrafica = new Anagrafica();
+        anagrafica.setCognome("Mario");
+        anagrafica.setNome("Bianchi");
+        credenziali = new Credenziali();
+        credenziali.setUsername("mario.bianchi");
+        credenziali.setPassword(credenziali.md5hash("mariobianchi"));
+        utente.setCredenziali(credenziali);
+        utente.setAnagrafica(anagrafica);
+        listaUtenti.add(utente);
+
+    }
 
 }
