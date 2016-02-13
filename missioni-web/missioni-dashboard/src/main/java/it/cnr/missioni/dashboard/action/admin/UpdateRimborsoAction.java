eb555a53f16de32bcd5481b6f050d2443e28a736
vito.salvia@gmail.com <vito.salvia@gmail.com>
2016-02-13 11:39:41 +0100

    new action for update rimborso

---
+++ missioni-web/missioni-dashboard/src/main/java/it/cnr/missioni/dashboard/action/admin/UpdateRimborsoAction.java
@@ -0,0 +1,46 @@
+package it.cnr.missioni.dashboard.action.admin;
+
+import org.joda.time.DateTime;
+
+import com.vaadin.ui.Notification.Type;
+
+import it.cnr.missioni.dashboard.action.IAction;
+import it.cnr.missioni.dashboard.client.ClientConnector;
+import it.cnr.missioni.dashboard.event.DashboardEvent;
+import it.cnr.missioni.dashboard.event.DashboardEventBus;
+import it.cnr.missioni.dashboard.utility.Utility;
+import it.cnr.missioni.model.missione.Missione;
+
+/**
+ * @author Salvia Vito
+ */
+public class UpdateRimborsoAction implements IAction {
+
+	private Missione missione;
+
+	public UpdateRimborsoAction(Missione missione) {
+		this.missione = missione;
+	}
+
+	public boolean doAction() {
+
+		try {
+
+			missione.getRimborso().setDateLastModified(new DateTime());
+			
+			ClientConnector.updateRimborso(missione);
+			Thread.sleep(1000);	
+			DashboardEventBus.post(new DashboardEvent.TableRimborsiUpdatedEvent());
+			Utility.getNotification(Utility.getMessage("success_message"), null, Type.HUMANIZED_MESSAGE);
+
+			return true;
+
+		} catch (Exception e) {
+			Utility.getNotification(Utility.getMessage("error_message"), Utility.getMessage("request_error"),
+					Type.ERROR_MESSAGE);
+			return false;
+		}
+
+	}
+
+}
