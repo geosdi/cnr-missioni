1e1da51481bd733a8697e7295c6224835f541a6d
vito.salvia@gmail.com <vito.salvia@gmail.com>
2016-02-29 12:14:36 +0100

    check isMezzoProprio

--- missioni-notification-support/missioni-mail-support/src/main/java/it/cnr/missioni/notification/support/itext/missione/MissionePDFBuilder.java
+++ missioni-notification-support/missioni-mail-support/src/main/java/it/cnr/missioni/notification/support/itext/missione/MissionePDFBuilder.java
@@ -183,7 +183,7 @@ public class MissionePDFBuilder extends PDFBuilder.AbstractPDFBuilder {
 		String tipoVeicolo = missione.isMezzoProprio() ? "Veicolo Proprio" : "Veicolo CNR";
 
 		PdfPCell cellDisposizioni2 = new PdfPCell(new Paragraph(tipoVeicolo + " - "
-				+ (missione.getMotivazioniMezzoProprio().isEmpty() ? missione.getMotivazioniMezzoProprio() : "" ),
+				+ (missione.getMotivazioniMezzoProprio() != null ? missione.getMotivazioniMezzoProprio() : "" ),
 				fontNormal));
 		// cellOggetto2.setNoWrap(false);
 		tableLocalita.addCell(cellDisposizioni2);
