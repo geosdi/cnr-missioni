06a853a394e3d9d7367c7271834f120fea840aef
vito.salvia@gmail.com <vito.salvia@gmail.com>
2016-02-29 12:16:46 +0100

    add new form

---
+++ missioni-web/missioni-dashboard/src/main/java/it/cnr/missioni/dashboard/component/form/nazione/NazioneForm.java
@@ -0,0 +1,78 @@
+package it.cnr.missioni.dashboard.component.form.nazione;
+
+import com.vaadin.data.Validator.InvalidValueException;
+import com.vaadin.data.fieldgroup.BeanFieldGroup;
+import com.vaadin.data.fieldgroup.FieldGroup.CommitException;
+import com.vaadin.data.util.BeanItem;
+import com.vaadin.ui.AbstractField;
+import com.vaadin.ui.ComboBox;
+import com.vaadin.ui.Field;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.themes.ValoTheme;
+
+import it.cnr.missioni.dashboard.component.form.IForm;
+import it.cnr.missioni.model.configuration.Nazione;
+
+/**
+ * @author Salvia Vito
+ */
+public class NazioneForm extends IForm.FormAbstract<Nazione> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5674917270638827694L;
+
+	private TextField valueField;
+	private ComboBox areagGeograficaField;
+
+
+	public NazioneForm(Nazione nazione,boolean isAdmin,boolean enabled,boolean modifica) {
+		super(nazione,isAdmin,enabled,modifica);
+		setFieldGroup(new BeanFieldGroup<Nazione>(Nazione.class));
+		buildFieldGroup();
+		buildTab();
+
+	}
+
+
+	public void  buildTab() {
+		addStyleName(ValoTheme.FORMLAYOUT_LIGHT);
+		valueField = (TextField) getFieldGroup().buildAndBind("Nazione", "value");
+		addComponent(valueField);
+		areagGeograficaField = (ComboBox) getFieldGroup().buildAndBind("Area Geografica", "areaGeografica", ComboBox.class);
+		addComponent(areagGeograficaField);
+	}
+
+	public Nazione validate() throws CommitException,InvalidValueException {
+		for (Field<?> f : getFieldGroup().getFields()) {
+			((AbstractField<?>) f).setValidationVisible(true);
+		}
+		getFieldGroup().commit();
+
+		BeanItem<Nazione> beanItem = (BeanItem<Nazione>) getFieldGroup().getItemDataSource();
+		Nazione nazione = beanItem.getBean();
+		return nazione;
+	}
+
+
+	/**
+	 * 
+	 */
+	@Override
+	public void addValidator() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	/**
+	 * 
+	 */
+	@Override
+	public void addListener() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
