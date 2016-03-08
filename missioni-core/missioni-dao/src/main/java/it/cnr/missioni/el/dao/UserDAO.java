c71a13256f19558d75d737867171cdd1cc51cabc
vito.salvia@gmail.com <vito.salvia@gmail.com>
2016-03-08 11:52:33 +0100

    refactoring

--- missioni-core/missioni-dao/src/main/java/it/cnr/missioni/el/dao/UserDAO.java
+++ missioni-core/missioni-dao/src/main/java/it/cnr/missioni/el/dao/UserDAO.java
@@ -35,7 +35,7 @@ public class UserDAO extends AbstractElasticSearchDAO<User> implements IUserDAO 
 
 		List<User> listaUsers = new ArrayList<User>();
 		logger.debug("###############Try to find Users by Query: {}\n\n");
-
+		
 		Page p = new Page(userSearchBuilder.getFrom(), userSearchBuilder.isAll() ? count().intValue():userSearchBuilder.getSize());
 
 		SearchResponse searchResponse = p
