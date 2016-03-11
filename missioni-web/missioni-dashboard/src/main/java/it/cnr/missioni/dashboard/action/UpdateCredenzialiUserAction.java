4825f58fa03beaed63474eb78c9c3c9f441a859a
vito.salvia@gmail.com <vito.salvia@gmail.com>
2016-03-11 20:28:18 +0100

    add action

---
+++ missioni-web/missioni-dashboard/src/main/java/it/cnr/missioni/dashboard/action/UpdateCredenzialiUserAction.java
@@ -0,0 +1,39 @@
+package it.cnr.missioni.dashboard.action;
+
+import com.vaadin.ui.Notification.Type;
+
+import it.cnr.missioni.dashboard.client.ClientConnector;
+import it.cnr.missioni.dashboard.utility.Utility;
+import it.cnr.missioni.model.user.User;
+
+/**
+ * @author Salvia Vito
+ */
+public class UpdateCredenzialiUserAction implements IAction {
+
+	private User user;
+
+	public UpdateCredenzialiUserAction(User user ) {
+		this.user = user;
+	}
+
+	public boolean doAction() {
+
+		try {
+			user.getCredenziali().setPassword(user.getCredenziali().md5hash(user.getCredenziali().getPassword()));
+			ClientConnector.updateUser(user);
+
+			Utility.getNotification(Utility.getMessage("success_message"), null,
+					Type.HUMANIZED_MESSAGE);
+
+			return true;
+
+		} catch (Exception e) {
+			Utility.getNotification(Utility.getMessage("error_message"), null,
+					Type.ERROR_MESSAGE);
+			return false;
+		}
+
+	}
+
+}
