e5a6c8cc2786234e2ed89f9272898366144d29ea
vito.salvia@gmail.com <vito.salvia@gmail.com>
2016-03-22 14:32:48 +0100

    refactoring

--- missioni-web/missioni-dashboard/src/main/java/it/cnr/missioni/dashboard/component/form/missione/LocalitaOggettoMissioneForm.java
+++ missioni-web/missioni-dashboard/src/main/java/it/cnr/missioni/dashboard/component/form/missione/LocalitaOggettoMissioneForm.java
@@ -32,264 +32,263 @@ import it.cnr.missioni.rest.api.response.nazione.NazioneStore;
  */
 public class LocalitaOggettoMissioneForm extends IForm.FormAbstract<Missione> {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -8610795278738351308L;
-	private TextField localitaField;
-	private TextField oggettoField;
-	private ComboBox listaLocalitaField;
-	private GeocoderStore geocoderStore;
-	private TextField distanzaField;
-	private ComboBox listaNazioneField;
-	private ComboBox listaStatoField;
+    /**
+     *
+     */
+    private static final long serialVersionUID = -8610795278738351308L;
+    private TextField localitaField;
+    private TextField oggettoField;
+    private ComboBox listaLocalitaField;
+    private GeocoderStore geocoderStore;
+    private TextField distanzaField;
+    private ComboBox listaNazioneField;
+    private ComboBox listaStatoField;
 
-	public LocalitaOggettoMissioneForm(Missione missione, boolean isAdmin, boolean enabled, boolean modifica) {
-		super(missione, isAdmin, enabled, modifica);
-		setFieldGroup(new BeanFieldGroup<Missione>(Missione.class));
-		buildFieldGroup();
-		buildTab();
+    public LocalitaOggettoMissioneForm(Missione missione, boolean isAdmin, boolean enabled, boolean modifica) {
+        super(missione, isAdmin, enabled, modifica);
+        setFieldGroup(new BeanFieldGroup<Missione>(Missione.class));
+        buildFieldGroup();
+        buildTab();
 
-	}
+    }
 
-	public void buildTab() {
+    public void buildTab() {
 
-		
-		listaStatoField = (ComboBox)getFieldGroup().buildAndBind("Stato","stato",ComboBox.class);
 
-		
-		localitaField = new TextField("Localita");
-		listaLocalitaField = new ComboBox("Seleziona località");
-		listaLocalitaField.setValidationVisible(false);
-		if(modifica){
-			listaLocalitaField.addItem(bean.getGeoPoint().getLat() + "/" + bean.getGeoPoint().getLon());
-			listaLocalitaField.setItemCaption(bean.getGeoPoint().getLat() + "/" + bean.getGeoPoint().getLon(),
-					bean.getLocalita());
-			listaLocalitaField.select(bean.getGeoPoint().getLat() + "/" + bean.getGeoPoint().getLon());
-			if(!isAdmin)
-				listaLocalitaField.setReadOnly(true);
+        listaStatoField = (ComboBox) getFieldGroup().buildAndBind("Stato", "stato", ComboBox.class);
 
-		}
-		
-		
-		oggettoField = (TextField) getFieldGroup().buildAndBind("Oggetto", "oggetto");
-		distanzaField = (TextField) getFieldGroup().buildAndBind("Distanza", "distanza");
-		distanzaField.setEnabled(false);
-		buildFields();
-		HorizontalLayout layoutLocalita = new HorizontalLayout();
-		layoutLocalita.addComponent(localitaField);
-		if(isAdmin)
-			addComponent(listaStatoField);
-		addComponent(localitaField);
-		addComponent(listaLocalitaField);
-		addComponent(oggettoField);
-		addComponent(distanzaField);
-		localitaField.setReadOnly(!enabled);
-		addComponent(listaNazioneField);
-		addListener();
-		addValidator();
 
-	}
+        localitaField = new TextField("Localita");
+        listaLocalitaField = new ComboBox("Seleziona località");
+        listaLocalitaField.setValidationVisible(false);
+        if (modifica) {
+            listaLocalitaField.addItem(bean.getGeoPoint().getLat() + "/" + bean.getGeoPoint().getLon());
+            listaLocalitaField.setItemCaption(bean.getGeoPoint().getLat() + "/" + bean.getGeoPoint().getLon(),
+                    bean.getLocalita());
+            listaLocalitaField.select(bean.getGeoPoint().getLat() + "/" + bean.getGeoPoint().getLon());
+            if (!isAdmin)
+                listaLocalitaField.setReadOnly(true);
 
-	private void buildFields(){
-		listaNazioneField = new ComboBox("Nazione");
-		listaNazioneField.setImmediate(true);
-		listaNazioneField.setValidationVisible(false);
-		getFieldGroup().bind(listaNazioneField, "idNazione");
-		try {
-			NazioneStore nazioneStore = ClientConnector
-					.getNazione(NazioneSearchBuilder.getNazioneSearchBuilder().withAll(true));
-			nazioneStore.getNazione().forEach(c -> {
-				listaNazioneField.addItem(c.getId());
-				listaNazioneField.setItemCaption(c.getId(), c.getValue());
-			});
-		} catch (Exception e) {
-			Utility.getNotification(Utility.getMessage("error_message"), Utility.getMessage("request_error"),
-					Type.ERROR_MESSAGE);
-		}
-	}
-	
-	public void setVisibleField(boolean visible){
-		listaNazioneField.setVisible(visible);
-	}
-	
-	public void addListener() {
+        }
 
-		listaLocalitaField.addBlurListener(new BlurListener() {
 
-			/**
-			 * 
-			 */
-			private static final long serialVersionUID = 7311640376669034824L;
+        oggettoField = (TextField) getFieldGroup().buildAndBind("Oggetto", "oggetto");
+        distanzaField = (TextField) getFieldGroup().buildAndBind("Distanza", "distanza");
+        distanzaField.setEnabled(false);
+        buildFields();
+        HorizontalLayout layoutLocalita = new HorizontalLayout();
+        layoutLocalita.addComponent(localitaField);
+        if (isAdmin)
+            addComponent(listaStatoField);
+        addComponent(localitaField);
+        addComponent(listaLocalitaField);
+        addComponent(oggettoField);
+        addComponent(distanzaField);
+        localitaField.setReadOnly(!enabled);
+        addComponent(listaNazioneField);
+        addListener();
+        addValidator();
 
-			@Override
-			public void blur(BlurEvent event) {
-				try {
-					listaLocalitaField.validate();
-				} catch (Exception e) {
-					listaLocalitaField.setValidationVisible(true);
-				}
-			}
-		});
+    }
 
-		listaNazioneField.addBlurListener(new BlurListener() {
+    private void buildFields() {
+        listaNazioneField = new ComboBox("Nazione");
+        listaNazioneField.setImmediate(true);
+        listaNazioneField.setValidationVisible(false);
+        getFieldGroup().bind(listaNazioneField, "idNazione");
+        try {
+            NazioneStore nazioneStore = ClientConnector
+                    .getNazione(NazioneSearchBuilder.getNazioneSearchBuilder().withAll(true));
+            nazioneStore.getNazione().forEach(c -> {
+                listaNazioneField.addItem(c.getId());
+                listaNazioneField.setItemCaption(c.getId(), c.getValue());
+            });
+        } catch (Exception e) {
+            Utility.getNotification(Utility.getMessage("error_message"), Utility.getMessage("request_error"),
+                    Type.ERROR_MESSAGE);
+        }
+    }
 
-			/**
-			 * 
-			 */
-			private static final long serialVersionUID = 7922449070519357247L;
+    public void setVisibleField(boolean visible) {
+        listaNazioneField.setVisible(visible);
+    }
 
-			@Override
-			public void blur(BlurEvent event) {
-				try {
-					listaNazioneField.validate();
-				} catch (Exception e) {
-					listaNazioneField.setValidationVisible(true);
-				}
-			}
-		});
+    public void addListener() {
 
-		listaLocalitaField.addBlurListener(new BlurListener() {
+        listaLocalitaField.addBlurListener(new BlurListener() {
 
-			/**
-			 * 
-			 */
-			private static final long serialVersionUID = 1427910449983002609L;
+            /**
+             *
+             */
+            private static final long serialVersionUID = 7311640376669034824L;
 
-			@Override
-			public void blur(BlurEvent event) {
-				try {
+            @Override
+            public void blur(BlurEvent event) {
+                try {
+                    listaLocalitaField.validate();
+                } catch (Exception e) {
+                    listaLocalitaField.setValidationVisible(true);
+                }
+            }
+        });
 
-					String localita = listaLocalitaField.getItemCaption(listaLocalitaField.getValue());
-					if (localita != null) {
-						DistanceResponse.MissioneDistanceResponse distance = ClientConnector
-								.getDistanceForMissione("Tito Scalo", localita);
-						distanzaField.setValue(distance.getDistance());
-					} else {
-						distanzaField.setValue(null);
-					}
-				} catch (Exception e) {
-					Utility.getNotification(Utility.getMessage("error_message"), Utility.getMessage("request_error"),
-							Type.ERROR_MESSAGE);
-				}
-			}
+        listaNazioneField.addBlurListener(new BlurListener() {
 
-		});
+            /**
+             *
+             */
+            private static final long serialVersionUID = 7922449070519357247L;
 
-		localitaField.addValueChangeListener(new ValueChangeListener() {
+            @Override
+            public void blur(BlurEvent event) {
+                try {
+                    listaNazioneField.validate();
+                } catch (Exception e) {
+                    listaNazioneField.setValidationVisible(true);
+                }
+            }
+        });
 
-			/**
-			 * 
-			 */
-			private static final long serialVersionUID = -3920108318148178176L;
+        listaLocalitaField.addBlurListener(new BlurListener() {
 
-			@Override
-			public void valueChange(ValueChangeEvent event) {
+            /**
+             *
+             */
+            private static final long serialVersionUID = 1427910449983002609L;
 
-				listaLocalitaField.removeAllItems();
+            @Override
+            public void blur(BlurEvent event) {
+                try {
 
-				if (!localitaField.getValue().trim().equals("")) {
+                    String localita = listaLocalitaField.getItemCaption(listaLocalitaField.getValue());
+                    if (localita != null) {
+                        DistanceResponse.MissioneDistanceResponse distance = ClientConnector
+                                .getDistanceForMissione("CNR-IMAA, Strada Per Contrada Petrucco, 85050 Zona Industriale PZ", localita);
+                        distanzaField.setValue(distance.getDistance());
+                    } else {
+                        distanzaField.setValue(null);
+                    }
+                } catch (Exception e) {
+                    Utility.getNotification(Utility.getMessage("error_message"), Utility.getMessage("request_error"),
+                            Type.ERROR_MESSAGE);
+                }
+            }
 
-					try {
-						geocoderStore = ClientConnector.getGeocoderStoreForMissioneLocation(localitaField.getValue());
+        });
 
-						if (geocoderStore.getGeocoderResponses() != null) {
+        localitaField.addValueChangeListener(new ValueChangeListener() {
 
-							geocoderStore.getGeocoderResponses().forEach(c -> {
-								listaLocalitaField.addItem(c.getLat() + "/" + c.getLon());
-								listaLocalitaField.setItemCaption(c.getLat() + "/" + c.getLon(),
-										c.getFormattedAddress());
-							});
-						} else {
-							Utility.getNotification(Utility.getMessage("error_message"),
-									Utility.getMessage("localita_error"), Type.ERROR_MESSAGE);
-						}
+            /**
+             *
+             */
+            private static final long serialVersionUID = -3920108318148178176L;
 
-					} catch (Exception e) {
-						Utility.getNotification(Utility.getMessage("error_message"),
-								Utility.getMessage("request_error"), Type.ERROR_MESSAGE);
-					}
-				}
+            @Override
+            public void valueChange(ValueChangeEvent event) {
 
-			}
-		});
-	}
+                listaLocalitaField.removeAllItems();
 
-	public Missione validate() throws CommitException, InvalidValueException {
+                if (!localitaField.getValue().trim().equals("")) {
 
-		for (Field<?> f : getFieldGroup().getFields()) {
-			((AbstractField<?>) f).setValidationVisible(true);
-		}
-		listaLocalitaField.setValidationVisible(true);
-		listaLocalitaField.validate();
-		oggettoField.validate();
+                    try {
+                        geocoderStore = ClientConnector.getGeocoderStoreForMissioneLocation(localitaField.getValue());
+
+                        if (geocoderStore.getGeocoderResponses() != null) {
+
+                            geocoderStore.getGeocoderResponses().forEach(c -> {
+                                listaLocalitaField.addItem(c.getLat() + "/" + c.getLon());
+                                listaLocalitaField.setItemCaption(c.getLat() + "/" + c.getLon(),
+                                        c.getFormattedAddress());
+                            });
+                        } else {
+                            Utility.getNotification(Utility.getMessage("error_message"),
+                                    Utility.getMessage("localita_error"), Type.ERROR_MESSAGE);
+                        }
+
+                    } catch (Exception e) {
+                        Utility.getNotification(Utility.getMessage("error_message"),
+                                Utility.getMessage("request_error"), Type.ERROR_MESSAGE);
+                    }
+                }
+
+            }
+        });
+    }
+
+    public Missione validate() throws CommitException, InvalidValueException {
+
+        for (Field<?> f : getFieldGroup().getFields()) {
+            ((AbstractField<?>) f).setValidationVisible(true);
+        }
+        listaLocalitaField.setValidationVisible(true);
+        listaLocalitaField.validate();
+        oggettoField.validate();
 //		if (bean.isMissioneEstera()) 
-		listaNazioneField.validate();
+        listaNazioneField.validate();
 
-		bean.setOggetto(oggettoField.getValue());
-		bean.setLocalita(listaLocalitaField.getItemCaption(listaLocalitaField.getValue()));
-		bean.setDistanza(distanzaField.getValue());
-		
-		//Missione estera
-		if (bean.isMissioneEstera()) {
-			bean.setIdNazione(listaNazioneField.getValue().toString());
-			bean.setShortDescriptionNazione(listaNazioneField.getItemCaption(bean.getIdNazione()));
-		}else{
-			bean.setIdNazione(null);
-			bean.setShortDescriptionNazione(null);
-		}
-		if(isAdmin)
-			bean.setStato(StatoEnum.getStatoE(listaStatoField.getValue().toString()));
+        bean.setOggetto(oggettoField.getValue());
+        bean.setLocalita(listaLocalitaField.getItemCaption(listaLocalitaField.getValue()));
+        bean.setDistanza(distanzaField.getValue());
 
-		String[] latLng = ((String) listaLocalitaField.getValue()).split("/");
-		GeoPoint geoPoint = new GeoPoint(Double.parseDouble(latLng[0]), Double.parseDouble(latLng[1]));
-		bean.setGeoPoint(geoPoint);
-		bean.setLocalita(listaLocalitaField.getItemCaption(listaLocalitaField.getValue()));
-		return bean;
-	}
+        //Missione estera
+        if (bean.isMissioneEstera()) {
+            bean.setIdNazione(listaNazioneField.getValue().toString());
+            bean.setShortDescriptionNazione(listaNazioneField.getItemCaption(bean.getIdNazione()));
+        } else {
+            bean.setIdNazione(null);
+            bean.setShortDescriptionNazione(null);
+        }
+        if (isAdmin)
+            bean.setStato(StatoEnum.getStatoE(listaStatoField.getValue().toString()));
 
-	/**
-	 * 
-	 */
-	@Override
-	public void addValidator() {
-		listaNazioneField.addValidator(new Validator() {
+        String[] latLng = ((String) listaLocalitaField.getValue()).split("/");
+        GeoPoint geoPoint = new GeoPoint(Double.parseDouble(latLng[0]), Double.parseDouble(latLng[1]));
+        bean.setGeoPoint(geoPoint);
+        bean.setLocalita(listaLocalitaField.getItemCaption(listaLocalitaField.getValue()));
+        return bean;
+    }
 
-			/**
-			 * 
-			 */
-			private static final long serialVersionUID = 2212244745005231496L;
+    /**
+     *
+     */
+    @Override
+    public void addValidator() {
+        listaNazioneField.addValidator(new Validator() {
 
-			@Override
-			public void validate(Object value) throws InvalidValueException {
+            /**
+             *
+             */
+            private static final long serialVersionUID = 2212244745005231496L;
 
-				if (value == null && bean.isMissioneEstera())
-					throw new InvalidValueException(Utility.getMessage("nazione_error"));
+            @Override
+            public void validate(Object value) throws InvalidValueException {
 
-			}
+                if (value == null && bean.isMissioneEstera())
+                    throw new InvalidValueException(Utility.getMessage("nazione_error"));
 
-		});
-		
-		
-		listaLocalitaField.addValidator(new Validator() {
+            }
+
+        });
 
 
+        listaLocalitaField.addValidator(new Validator() {
 
-			/**
-			 * 
-			 */
-			private static final long serialVersionUID = 621159309875209551L;
 
-			@Override
-			public void validate(Object value) throws InvalidValueException {
+            /**
+             *
+             */
+            private static final long serialVersionUID = 621159309875209551L;
 
-				if (value == null )
-					throw new InvalidValueException(Utility.getMessage("field_required"));
+            @Override
+            public void validate(Object value) throws InvalidValueException {
 
-			}
+                if (value == null)
+                    throw new InvalidValueException(Utility.getMessage("field_required"));
 
-		});
-		
-	}
+            }
+
+        });
+
+    }
 
 }
