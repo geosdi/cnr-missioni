d32f0dd8f775baa23b83606c4a108c026dcbb7be
vito.salvia@gmail.com <vito.salvia@gmail.com>
2016-03-26 18:08:30 +0100

    Added binder

---
+++ missioni-web/missioni-dashboard/src/main/java/it/cnr/missioni/dashboard/binder/IBinder.java
@@ -0,0 +1,32 @@
+package it.cnr.missioni.dashboard.binder;
+
+public interface IBinder<TO extends Object, FROM extends Object,B> {
+
+	B withFrom(FROM from);
+	
+	TO bind();
+
+	abstract class AbstractBinder<TO extends Object, FROM extends Object,B extends IBinder> implements IBinder<TO, FROM,B> {
+
+		
+		protected FROM from;
+		
+		/**
+		 * 
+		 */
+		protected AbstractBinder() {
+		}
+
+		/**
+		 * @param from
+		 */
+		@Override
+		public B withFrom(FROM from) {
+			this.from = from;
+			return self();
+		}
+
+		protected abstract B self();
+
+	}
+}
---
+++ missioni-web/missioni-dashboard/src/main/java/it/cnr/missioni/dashboard/binder/IPrenotazioneBinder.java
@@ -0,0 +1,96 @@
+package it.cnr.missioni.dashboard.binder;
+
+import java.util.function.Function;
+
+import org.joda.time.DateTime;
+
+import it.cnr.missioni.dashboard.component.calendar.PrenotazioneEvent;
+import it.cnr.missioni.model.prenotazione.Prenotazione;
+import it.cnr.missioni.model.user.User;
+
+/**
+ * @author Salvia Vito
+ */
+public interface IPrenotazioneBinder<TO extends Prenotazione, FROM extends PrenotazioneEvent> extends IBinder<TO, FROM, IPrenotazioneBinder<TO,FROM>>{
+	
+	IPrenotazioneBinder<TO,FROM> withModifica(boolean modifica);
+	
+	IPrenotazioneBinder<TO,FROM> withUser(User user);
+	
+	class PrenotazioneBinder extends IBinder.AbstractBinder<Prenotazione, PrenotazioneEvent, IPrenotazioneBinder<Prenotazione,PrenotazioneEvent>> implements IPrenotazioneBinder<Prenotazione,PrenotazioneEvent>{
+
+		private boolean modifica;
+		private User user;
+		
+		
+		public static PrenotazioneBinder getPrenotazioneBinder(){
+			return new PrenotazioneBinder();
+		}
+		
+		/**
+		 * @return
+		 */
+		@Override
+		public Prenotazione bind() {
+			return new PrenotazioneFunction().apply(from);
+		}
+
+		public PrenotazioneBinder withModifica(boolean modifica){
+			this.modifica = modifica;
+			return self();
+		}
+		
+		public PrenotazioneBinder withUser(User user){
+			this.user = user;
+			return self();
+		}
+		
+		/**
+		 * @return
+		 */
+		@Override
+		protected PrenotazioneBinder self() {
+			return this;
+		}
+
+		class PrenotazioneFunction implements Function<PrenotazioneEvent, Prenotazione> {
+		/**
+		 * @param t
+		 * @return
+		 */
+		@Override
+		public Prenotazione apply(PrenotazioneEvent prenotazioneEvent) {
+			return new Prenotazione() {
+				{
+					super.setDataFrom(new DateTime(prenotazioneEvent.getStart().getTime()));
+					super.setDataTo(new DateTime(prenotazioneEvent.getEnd().getTime()));
+					super.setIdVeicoloCNR(prenotazioneEvent.getVeicolo());
+					super.setAllDay(prenotazioneEvent.isAllDay());
+					super.setLocalita(prenotazioneEvent.getLocalita());
+					super.setDescriptionVeicoloCNR(prenotazioneEvent.getVeicoloDescription());
+					if(modifica){
+						super.setId(prenotazioneEvent.getId());
+						super.setIdUser(prenotazioneEvent.getIdUser());
+						super.setDescrizione(prenotazioneEvent.getDescrizione());
+					}else{
+						super.setIdUser(user.getId());
+						super.setDescrizione(user.getAnagrafica().getCognome()
+								+ " " + user.getAnagrafica().getNome());
+					}
+				}
+
+			};
+		}
+
+	}
+
+
+
+
+	}
+
+	
+}
+
+
+
