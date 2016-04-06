2c4f0edfd327537bab6c09f388baa2ddf60c9848
vito.salvia@gmail.com <vito.salvia@gmail.com>
2016-04-06 11:38:10 +0200

    Refactoring

--- missioni-web/missioni-dashboard/src/main/java/it/cnr/missioni/dashboard/OptimizedConnectorBundleLoaderFactory.java
+++ missioni-web/missioni-dashboard/src/main/java/it/cnr/missioni/dashboard/OptimizedConnectorBundleLoaderFactory.java
@@ -1,9 +1,5 @@
 package it.cnr.missioni.dashboard;
 
-import java.util.HashSet;
-import java.util.Set;
-
-
 import com.google.gwt.core.ext.typeinfo.JClassType;
 import com.vaadin.client.ui.button.ButtonConnector;
 import com.vaadin.client.ui.csslayout.CssLayoutConnector;
@@ -18,9 +14,13 @@ import com.vaadin.client.ui.window.WindowConnector;
 import com.vaadin.server.widgetsetutils.ConnectorBundleLoaderFactory;
 import com.vaadin.shared.ui.Connect.LoadStyle;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public final class OptimizedConnectorBundleLoaderFactory extends
         ConnectorBundleLoaderFactory {
     private final Set<String> eagerConnectors = new HashSet<String>();
+
     {
         eagerConnectors.add(PasswordFieldConnector.class.getName());
         eagerConnectors.add(VerticalLayoutConnector.class.getName());
--- missioni-web/missioni-dashboard/src/main/java/it/cnr/missioni/dashboard/notification/NotificationProvider.java
+++ missioni-web/missioni-dashboard/src/main/java/it/cnr/missioni/dashboard/notification/NotificationProvider.java
@@ -1,121 +1,110 @@
 package it.cnr.missioni.dashboard.notification;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import org.joda.time.DateTime;
-
 import com.google.common.base.Predicate;
 import com.google.common.collect.Collections2;
-import com.vaadin.server.VaadinSession;
 import com.vaadin.ui.Notification.Type;
-
-import ch.qos.logback.core.db.dialect.DBUtil;
 import it.cnr.missioni.dashboard.DashboardUI;
 import it.cnr.missioni.dashboard.client.ClientConnector;
 import it.cnr.missioni.dashboard.utility.Utility;
 import it.cnr.missioni.el.model.search.builder.IMissioneSearchBuilder;
 import it.cnr.missioni.model.missione.Missione;
 import it.cnr.missioni.model.missione.StatoEnum;
-import it.cnr.missioni.model.user.User;
 import it.cnr.missioni.rest.api.response.missione.MissioniStore;
+import org.joda.time.DateTime;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
- * 
  * @author Salvia Vito
- *
  */
 public class NotificationProvider implements INotificationProvider {
 
-	private  final List<DashboardNotification> notifications = new ArrayList<DashboardNotification>();
+    private final List<DashboardNotification> notifications = new ArrayList<DashboardNotification>();
 
-	/**
-	 * Initialize the data for this application.
-	 */
-	public NotificationProvider() {
-	}
-	
-	public void check(){
-		chechRegistrationComplete();
-	}
+    /**
+     * Initialize the data for this application.
+     */
+    public NotificationProvider() {
+    }
 
-	public void addPrenotazione(String prenotazione){
-		notifications.add(buildNotification(prenotazione,"Prenotazione"));
-	}
-	
-	private void chechRegistrationComplete() {
-		DashboardNotification n = null;
-		List<Missione> lista = getMissioneWithoutRimborso();
-		if (!isUserRegistrationComplete()) {
-			notifications.add(buildNotification("Dati User non completi","E' necessario completare la registrazione"));
-		}
-		lista.forEach(m ->{
-			notifications.add(buildNotification("Id Missione: "+m.getId(),"Missione senza Rimborso"));
-		});
-		
-	}
+    public void check() {
+        chechRegistrationComplete();
+    }
 
-	/**
-	 * 
-	 * Verifica che l'user che si è loggato abbia completato la registrazione
-	 * 
-	 * @return
-	 */
-	private boolean isUserRegistrationComplete() {
-		return DashboardUI.getCurrentUser().isRegistrazioneCompletata();
-	}
-	
-	private DashboardNotification buildNotification(String content,String title){
-		DashboardNotification n = new DashboardNotification();
-		n.setRead(false);
-		n.setTitle(title);
-		n.setContent(content);
-		return n;
-	}
-	
-	/**
-	 * 
-	 * Verifica che non ci siano missioni inserite più di un mese fa senza alcun rimborso
-	 * 
-	 * @return
-	 */
-	private List<Missione>  getMissioneWithoutRimborso(){
-		List<Missione> lista = new ArrayList<Missione>();
-		IMissioneSearchBuilder missioneSearchBuilder = IMissioneSearchBuilder.MissioneSearchBuilder.getMissioneSearchBuilder().withIdUser(DashboardUI.getCurrentUser().getId())
-				.withFieldNotExist("missione.rimborso")
-				.withStato(StatoEnum.INSERITA.name());
-		try {
-			MissioniStore missioniStore = ClientConnector.getMissione(missioneSearchBuilder);
-			if(missioniStore.getTotale() > 0)
-				lista = missioniStore.getMissioni().stream().filter(m -> m.getDatiPeriodoMissione().getFineMissione().isBefore(new DateTime().minusDays(15))).collect(Collectors.toList());
-		} catch (Exception e) {
-			Utility.getNotification(Utility.getMessage("error_message"), Utility.getMessage("request_error"),
-					Type.ERROR_MESSAGE);
-		}
-		return lista;
-		
-	}
+    public void addPrenotazione(String prenotazione) {
+        notifications.add(buildNotification(prenotazione, "Prenotazione"));
+    }
 
-	@Override
-	public int getUnreadNotificationsCount() {
-		Predicate<DashboardNotification> unreadPredicate = new Predicate<DashboardNotification>() {
-			@Override
-			public boolean apply(DashboardNotification input) {
-				return !input.isRead();
-			}
-		};
-		return Collections2.filter(notifications, unreadPredicate).size();
-	}
+    private void chechRegistrationComplete() {
+        DashboardNotification n = null;
+        List<Missione> lista = getMissioneWithoutRimborso();
+        if (!isUserRegistrationComplete()) {
+            notifications.add(buildNotification("Dati User non completi", "E' necessario completare la registrazione"));
+        }
+        lista.forEach(m -> {
+            notifications.add(buildNotification("Id Missione: " + m.getId(), "Missione senza Rimborso"));
+        });
+    }
 
-	@Override
-	public Collection<DashboardNotification> getNotifications() {
-		for (DashboardNotification notification : notifications) {
-			notification.setRead(true);
-		}
-		return Collections.unmodifiableCollection(notifications);
-	}
+    /**
+     * Verifica che l'user che si è loggato abbia completato la registrazione
+     *
+     * @return
+     */
+    private boolean isUserRegistrationComplete() {
+        return DashboardUI.getCurrentUser().isRegistrazioneCompletata();
+    }
+
+    private DashboardNotification buildNotification(String content, String title) {
+        DashboardNotification n = new DashboardNotification();
+        n.setRead(false);
+        n.setTitle(title);
+        n.setContent(content);
+        return n;
+    }
+
+    /**
+     * Verifica che non ci siano missioni inserite più di un mese fa senza alcun rimborso
+     *
+     * @return
+     */
+    private List<Missione> getMissioneWithoutRimborso() {
+        List<Missione> lista = new ArrayList<Missione>();
+        IMissioneSearchBuilder missioneSearchBuilder = IMissioneSearchBuilder.MissioneSearchBuilder.getMissioneSearchBuilder().withIdUser(DashboardUI.getCurrentUser().getId())
+                .withFieldNotExist("missione.rimborso")
+                .withStato(StatoEnum.INSERITA.name());
+        try {
+            MissioniStore missioniStore = ClientConnector.getMissione(missioneSearchBuilder);
+            if (missioniStore.getTotale() > 0)
+                lista = missioniStore.getMissioni().stream().filter(m -> m.getDatiPeriodoMissione().getFineMissione().isBefore(new DateTime().minusDays(15))).collect(Collectors.toList());
+        } catch (Exception e) {
+            Utility.getNotification(Utility.getMessage("error_message"), Utility.getMessage("request_error"),
+                    Type.ERROR_MESSAGE);
+        }
+        return lista;
+    }
+
+    @Override
+    public int getUnreadNotificationsCount() {
+        Predicate<DashboardNotification> unreadPredicate = new Predicate<DashboardNotification>() {
+            @Override
+            public boolean apply(DashboardNotification input) {
+                return !input.isRead();
+            }
+        };
+        return Collections2.filter(notifications, unreadPredicate).size();
+    }
+
+    @Override
+    public Collection<DashboardNotification> getNotifications() {
+        for (DashboardNotification notification : notifications) {
+            notification.setRead(true);
+        }
+        return Collections.unmodifiableCollection(notifications);
+    }
 
 }
