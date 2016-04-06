ce8f7378878499ed2f32ede870b42d95295f0e43
vito.salvia@gmail.com <vito.salvia@gmail.com>
2016-04-06 12:29:03 +0200

    Refactoring

--- missioni-web/missioni-dashboard/src/main/java/it/cnr/missioni/dashboard/action/UserRegistrationAction.java
+++ missioni-web/missioni-dashboard/src/main/java/it/cnr/missioni/dashboard/action/UserRegistrationAction.java
@@ -1,45 +1,37 @@
 package it.cnr.missioni.dashboard.action;
 
-import org.joda.time.DateTime;
-
 import com.vaadin.ui.Notification.Type;
-
 import it.cnr.missioni.dashboard.client.ClientConnector;
 import it.cnr.missioni.dashboard.utility.Utility;
 import it.cnr.missioni.model.user.RuoloUserEnum;
 import it.cnr.missioni.model.user.User;
+import org.joda.time.DateTime;
 
 /**
  * @author Salvia Vito
  */
 public class UserRegistrationAction implements IAction {
 
-	private User user;
+    private User user;
 
-	public UserRegistrationAction(User user) {
-		this.user = user;
-	}
+    public UserRegistrationAction(User user) {
+        this.user = user;
+    }
 
-	public boolean doAction() {
-
-		try {
-
-			user.setRegistrazioneCompletata(false);
-			user.setDataRegistrazione(new DateTime());
-			user.setDateLastModified(new DateTime());
-			user.getCredenziali().setPassword(user.getCredenziali().md5hash(user.getCredenziali().getPassword()));
-			user.getCredenziali().setRuoloUtente(RuoloUserEnum.UTENTE_SEMPLICE);
-			ClientConnector.addUser(user);
-			Utility.getNotification(Utility.getMessage("success_message"), null, Type.HUMANIZED_MESSAGE);
-
-			return true;
-
-		} catch (Exception e) {
-			Utility.getNotification(Utility.getMessage("error_message"), Utility.getMessage("request_error"),
-					Type.ERROR_MESSAGE);
-			return false;
-		}
-
-	}
-
+    public boolean doAction() {
+        try {
+            user.setRegistrazioneCompletata(false);
+            user.setDataRegistrazione(new DateTime());
+            user.setDateLastModified(new DateTime());
+            user.getCredenziali().setPassword(user.getCredenziali().md5hash(user.getCredenziali().getPassword()));
+            user.getCredenziali().setRuoloUtente(RuoloUserEnum.UTENTE_SEMPLICE);
+            ClientConnector.addUser(user);
+            Utility.getNotification(Utility.getMessage("success_message"), null, Type.HUMANIZED_MESSAGE);
+            return true;
+        } catch (Exception e) {
+            Utility.getNotification(Utility.getMessage("error_message"), Utility.getMessage("request_error"),
+                    Type.ERROR_MESSAGE);
+            return false;
+        }
+    }
 }
