a767776a68cb8c9156b24826e48452fefc88a40f
vito.salvia@gmail.com <vito.salvia@gmail.com>
2016-04-06 12:20:37 +0200

    Refactoring and calling window improvements

--- missioni-web/missioni-dashboard/src/main/java/it/cnr/missioni/dashboard/component/wizard/user/PatenteUserStep.java
+++ missioni-web/missioni-dashboard/src/main/java/it/cnr/missioni/dashboard/component/wizard/user/PatenteUserStep.java
@@ -1,52 +1,56 @@
 package it.cnr.missioni.dashboard.component.wizard.user;
 
-import org.vaadin.teemu.wizards.WizardStep;
-
 import com.vaadin.ui.Component;
 import com.vaadin.ui.Notification.Type;
-
-import it.cnr.missioni.dashboard.component.form.user.PatenteUserForm;
+import it.cnr.missioni.dashboard.component.form.user.IPatenteUserForm;
+import it.cnr.missioni.dashboard.component.form.user.IPatenteUserForm.PatenteUserForm;
 import it.cnr.missioni.dashboard.utility.Utility;
 import it.cnr.missioni.model.user.Patente;
 import it.cnr.missioni.model.user.User;
+import org.vaadin.teemu.wizards.WizardStep;
 
 /**
  * @author Salvia Vito
  */
 public class PatenteUserStep implements WizardStep {
 
-	private User user;
-	private PatenteUserForm patenteTabLayout;
+    private User user;
+    private IPatenteUserForm patenteTabLayout;
 
-	public String getCaption() {
-		return "Step 3";
-	}
+    public PatenteUserStep(User user) {
+        this.user = user;
+//		this.patenteTabLayout = new PatenteUserForm(user, false,true,false);
+        this.patenteTabLayout = PatenteUserForm.getPatenteUserForm()
+                .withUser(user)
+                .withBean(user.getPatente())
+                .withIsAdmin(false)
+                .withEnabled(true)
+                .withModifica(false).build();
+    }
 
-	public PatenteUserStep(User user) {
-		this.user = user;
-		this.patenteTabLayout = new PatenteUserForm(user, false,true,false);
+    public String getCaption() {
+        return "Step 3";
+    }
 
-	}
+    public Component getContent() {
+        return this.patenteTabLayout;
+    }
 
-	public Component getContent() {
-		return this.patenteTabLayout;
-	}
+    public boolean onAdvance() {
+        try {
+            Patente patente = patenteTabLayout.validate();
+            if (patente != null)
+                user.setPatente(patente);
+            return true;
+        } catch (Exception e) {
+            Utility.getNotification(Utility.getMessage("error_message"), Utility.getMessage("request_error"),
+                    Type.ERROR_MESSAGE);
+            return false;
+        }
+    }
 
-	public boolean onAdvance() {
-		try {
-			Patente patente = patenteTabLayout.validate();
-			if (patente != null)
-				user.setPatente(patente);
-			return true;
-		} catch (Exception e) {
-			Utility.getNotification(Utility.getMessage("error_message"), Utility.getMessage("request_error"),
-					Type.ERROR_MESSAGE);
-			return false;
-		}
-	}
-
-	public boolean onBack() {
-		return true;
-	}
+    public boolean onBack() {
+        return true;
+    }
 
 }
