9fd71081a9dd40c7b61903b3751227a6c117f9ad
vito.salvia@gmail.com <vito.salvia@gmail.com>
2016-04-06 12:25:33 +0200

    Improvements

--- missioni-web/missioni-dashboard/src/main/java/it/cnr/missioni/dashboard/view/PrenotazioneVeicoloCNRView.java
+++ missioni-web/missioni-dashboard/src/main/java/it/cnr/missioni/dashboard/view/PrenotazioneVeicoloCNRView.java
@@ -5,48 +5,41 @@ import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.server.Responsive;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.ValoTheme;
-
 import it.cnr.missioni.dashboard.component.calendar.CalendarPrenotazioni;
 import it.cnr.missioni.dashboard.event.DashboardEventBus;
 
 
-public final class PrenotazioneVeicoloCNRView extends VerticalLayout implements View{
+public final class PrenotazioneVeicoloCNRView extends VerticalLayout implements View {
 
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -3779182500846329663L;
+    /**
+     *
+     */
+    private static final long serialVersionUID = -3779182500846329663L;
 
 
-	// private CssLayout panel = new CssLayout();
+    // private CssLayout panel = new CssLayout();
 
-	public PrenotazioneVeicoloCNRView() {
-		addStyleName(ValoTheme.PANEL_BORDERLESS);
-		addStyleName("missione-view");
-		setSizeFull();
-		DashboardEventBus.register(this);
-		setHeight("96%");
-		setWidth("98%");
-		addStyleName(ValoTheme.LAYOUT_CARD);
-		addStyleName("panel-view");
-		Responsive.makeResponsive(this);
-
-        CalendarPrenotazioni calendar = new CalendarPrenotazioni(null,null);
+    public PrenotazioneVeicoloCNRView() {
+        addStyleName(ValoTheme.PANEL_BORDERLESS);
+        addStyleName("missione-view");
+        setSizeFull();
+        DashboardEventBus.register(this);
+        setHeight("96%");
+        setWidth("98%");
+        addStyleName(ValoTheme.LAYOUT_CARD);
+        addStyleName("panel-view");
+        Responsive.makeResponsive(this);
+        CalendarPrenotazioni calendar = new CalendarPrenotazioni(null, null);
         calendar.initContent();
         addComponent(calendar.getLayoutCalendar());
-	}
+    }
 
-
-	/**
-	 * @param event
-	 */
-	@Override
-	public void enter(ViewChangeEvent event) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	
+    /**
+     * @param event
+     */
+    @Override
+    public void enter(ViewChangeEvent event) {
+    }
 
 }
