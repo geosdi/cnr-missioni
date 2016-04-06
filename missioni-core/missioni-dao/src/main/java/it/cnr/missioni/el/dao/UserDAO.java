503196747dba240740014f4a0cbe4b8323979604
vito.salvia@gmail.com <vito.salvia@gmail.com>
2016-04-06 12:28:47 +0200

    Refactoring

--- missioni-core/missioni-dao/src/main/java/it/cnr/missioni/el/dao/UserDAO.java
+++ missioni-core/missioni-dao/src/main/java/it/cnr/missioni/el/dao/UserDAO.java
@@ -1,10 +1,7 @@
 package it.cnr.missioni.el.dao;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.annotation.Resource;
-
+import it.cnr.missioni.el.model.search.builder.IUserSearchBuilder;
+import it.cnr.missioni.model.user.User;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.search.SearchHit;
@@ -14,8 +11,9 @@ import org.geosdi.geoplatform.experimental.el.dao.PageResult;
 import org.geosdi.geoplatform.experimental.el.index.GPIndexCreator;
 import org.springframework.stereotype.Component;
 
-import it.cnr.missioni.el.model.search.builder.IUserSearchBuilder;
-import it.cnr.missioni.model.user.User;
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Salvia Vito
@@ -23,57 +21,56 @@ import it.cnr.missioni.model.user.User;
 @Component(value = "userDAO")
 public class UserDAO extends AbstractElasticSearchDAO<User> implements IUserDAO {
 
-	/**
-	 * 
-	 * @param p
-	 * @param userSearchBuilder
-	 * @return
-	 * @throws Exception
-	 */
-	@Override
-	public PageResult<User> findUserByQuery(IUserSearchBuilder userSearchBuilder) throws Exception {
+    /**
+     * @param p
+     * @param userSearchBuilder
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public PageResult<User> findUserByQuery(IUserSearchBuilder userSearchBuilder) throws Exception {
 
-		List<User> listaUsers = new ArrayList<User>();
-		logger.debug("###############Try to find Users by Query: {}\n\n");
-		
-		Page p = new Page(userSearchBuilder.getFrom(), userSearchBuilder.isAll() ? count().intValue():userSearchBuilder.getSize());
+        List<User> listaUsers = new ArrayList<User>();
+        logger.debug("###############Try to find Users by Query: {}\n\n");
 
-		SearchResponse searchResponse = p
-				.buildPage(this.elastichSearchClient.prepareSearch(getIndexName()).setTypes(getIndexType())
-						.setQuery(userSearchBuilder.buildQuery()))
-				.addSort(userSearchBuilder.getFieldSort(), userSearchBuilder.getSortOrder()).execute().actionGet();
+        Page p = new Page(userSearchBuilder.getFrom(), userSearchBuilder.isAll() ? count().intValue() : userSearchBuilder.getSize());
 
-		if (searchResponse.status() != RestStatus.OK) {
-			throw new IllegalStateException("Error in Elastic Search Query.");
-		}
+        SearchResponse searchResponse = p
+                .buildPage(this.elastichSearchClient.prepareSearch(getIndexName()).setTypes(getIndexType())
+                        .setQuery(userSearchBuilder.buildQuery()))
+                .addSort(userSearchBuilder.getFieldSort(), userSearchBuilder.getSortOrder()).execute().actionGet();
 
-		for (SearchHit searchHit : searchResponse.getHits().hits()) {
-			User user = this.mapper.read(searchHit.getSourceAsString());
-			if (!user.isIdSetted()) {
-				user.setId(searchHit.getId());
-			}
-			listaUsers.add(user);
-		}
+        if (searchResponse.status() != RestStatus.OK) {
+            throw new IllegalStateException("Error in Elastic Search Query.");
+        }
 
-		return new PageResult<User>(searchResponse.getHits().getTotalHits(), listaUsers);
-	}
+        for (SearchHit searchHit : searchResponse.getHits().hits()) {
+            User user = this.mapper.read(searchHit.getSourceAsString());
+            if (!user.isIdSetted()) {
+                user.setId(searchHit.getId());
+            }
+            listaUsers.add(user);
+        }
 
-	@Override
-	public List<User> findLasts() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        return new PageResult<User>(searchResponse.getHits().getTotalHits(), listaUsers);
+    }
 
-	@Resource(name = "userMapper")
-	@Override
-	public <Mapper extends GPBaseMapper<User>> void setMapper(Mapper theMapper) {
-		this.mapper = theMapper;
-	}
+    @Override
+    public List<User> findLasts() throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Resource(name = "userIndexCreator")
-	@Override
-	public <IC extends GPIndexCreator> void setIndexCreator(IC ic) {
-		super.setIndexCreator(ic);
-	}
+    @Resource(name = "userMapper")
+    @Override
+    public <Mapper extends GPBaseMapper<User>> void setMapper(Mapper theMapper) {
+        this.mapper = theMapper;
+    }
+
+    @Resource(name = "userIndexCreator")
+    @Override
+    public <IC extends GPIndexCreator> void setIndexCreator(IC ic) {
+        super.setIndexCreator(ic);
+    }
 
 }
