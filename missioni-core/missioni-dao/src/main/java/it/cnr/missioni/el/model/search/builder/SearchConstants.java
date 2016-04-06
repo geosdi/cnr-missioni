3c03b30bcab19f43cecdf639497468d6e01733ec
vito.salvia@gmail.com <vito.salvia@gmail.com>
2016-04-06 12:26:28 +0200

    Refactoring

--- missioni-core/missioni-dao/src/main/java/it/cnr/missioni/el/model/search/builder/SearchConstants.java
+++ missioni-core/missioni-dao/src/main/java/it/cnr/missioni/el/model/search/builder/SearchConstants.java
@@ -5,65 +5,65 @@ package it.cnr.missioni.el.model.search.builder;
  */
 public class SearchConstants {
 
-	public final static String USER_FIELD_NOME = "user.anagrafica.nome";
-	public final static String USER_FIELD_COGNOME = "user.anagrafica.cognome";
-	public final static String USER_FIELD_CODICE_FISCALE = "user.anagrafica.codiceFiscale";
-	public final static String USER_FIELD_USERNAME = "user.credenziali.username";
-	public final static String USER_FIELD_NUMERO_PATENTE = "user.patente.numeroPatente";
-	public final static String USER_FIELD_RESPONSABILIE_GRUPPO = "user.responsabileGruppo";
-	public final static String USER_FIELD_MATRICOLA = "user.datiCNR.matricola";
-	public final static String USER_FIELD_IBAN = "user.datiCNR.iban";
-	public final static String USER_FIELD_MAIL = "user.datiCNR.mail";
-	public final static String USER_FIELD_VEICOLO_TARGA = "user.mappaVeicolo.entry.value.targa";
-	public final static String USER_FIELD_VEICOLO_CARTA_CIRCOLAZIONE = "user.mappaVeicolo.entry.value.cartaCircolazione";
-	public final static String USER_FIELD_VEICOLO_POLIZZA_ASSICURATIVA = "user.mappaVeicolo.entry.value.polizzaAssicurativa";
-	public final static String USER_FIELD_ID = "user.id";
-	
-	
-	public static final String MISSIONE_FIELD_ID= "missione.id";
-	public static final String MISSIONE_FIELD_OGGETTO= "missione.oggetto";
-	public static final String MISSIONE_FIELD_DATA_INSERIMENTO = "missione.dataInserimento";
-	public static final String MISSIONE_FIELD_ID_USER = "missione.idUser";
-	public static final String MISSIONE_FIELD_STATO = "missione.stato";
-	public static final String MISSIONE_FIELD_RIMBORSO_NUMERO_ORDINE = "missione.rimborso.numeroOrdine";
-	public static final String MISSIONE_FIELD_RIMBORSO_DATA_RIMBORSO = "missione.rimborso.dataRimborso";
-	public final static String MISSIONE_FIELD_RIMBORSO = "missione.rimborso";
-
-	
-	public static final String PRENOTAZIONE_FIELD_DATA_FROM = "prenotazione.dataFrom";
-
-	public static final String VEICOLO_CNR_FIELD_STATO = "veicoloCNR.stato";
-	public static final String VEICOLO_CNR_FIELD_TARGA = "veicoloCNR.targa";
-	public static final String VEICOLO_CNR_FIELD_CARTA_CIRCOLAZIONE = "veicoloCNR.cartaCircolazione";
-	public static final String VEICOLO_CNR_FIELD_POLIZZA_ASSICURATIVA = "veicoloCNR.polizzaAssicurativa";
-	public static final String VEICOLO_CNR_FIELD_ID = "veicoloCNR.id";
-	public static final String VEICOLO_CNR_FIELD_TIPO = "veicoloCNR.tipo";
+    public final static String USER_FIELD_NOME = "user.anagrafica.nome";
+    public final static String USER_FIELD_COGNOME = "user.anagrafica.cognome";
+    public final static String USER_FIELD_CODICE_FISCALE = "user.anagrafica.codiceFiscale";
+    public final static String USER_FIELD_USERNAME = "user.credenziali.username";
+    public final static String USER_FIELD_NUMERO_PATENTE = "user.patente.numeroPatente";
+    public final static String USER_FIELD_RESPONSABILIE_GRUPPO = "user.responsabileGruppo";
+    public final static String USER_FIELD_MATRICOLA = "user.datiCNR.matricola";
+    public final static String USER_FIELD_IBAN = "user.datiCNR.iban";
+    public final static String USER_FIELD_MAIL = "user.datiCNR.mail";
+    public final static String USER_FIELD_VEICOLO_TARGA = "user.mappaVeicolo.entry.value.targa";
+    public final static String USER_FIELD_VEICOLO_CARTA_CIRCOLAZIONE = "user.mappaVeicolo.entry.value.cartaCircolazione";
+    public final static String USER_FIELD_VEICOLO_POLIZZA_ASSICURATIVA = "user.mappaVeicolo.entry.value.polizzaAssicurativa";
+    public final static String USER_FIELD_ID = "user.id";
 
 
-	public static final String QUALIFICA_USER_FIELD_VALUE = "qualificaUser.value";
-	public static final String QUALIFICA_USER_FIELD_ID = "qualificaUser.id";
-	
-	public static final String RIMBORSO_KM_FIELD_VALUE = "rimborsoKm.value";
-	public static final String RIMBORSO_KM_FIELD_ID = "rimborsoKm.id";
-	
-	
-	public static final String TIPOLOGIA_SPESA_FIELD_VALUE = "tipologiaSpesa.value";
-	public static final String TIPOLOGIA_SPESA_FIELD_ID = "tipologiaSpesa.id";
-	public static final String TIPOLOGIA_SPESA_FIELD_ESTERA = "tipologiaSpesa.estera";
-	public static final String TIPOLOGIA_SPESA_FIELD_ITALIA = "tipologiaSpesa.italia";
+    public static final String MISSIONE_FIELD_ID = "missione.id";
+    public static final String MISSIONE_FIELD_OGGETTO = "missione.oggetto";
+    public static final String MISSIONE_FIELD_DATA_INSERIMENTO = "missione.dataInserimento";
+    public static final String MISSIONE_FIELD_ID_USER = "missione.idUser";
+    public static final String MISSIONE_FIELD_STATO = "missione.stato";
+    public static final String MISSIONE_FIELD_RIMBORSO_NUMERO_ORDINE = "missione.rimborso.numeroOrdine";
+    public static final String MISSIONE_FIELD_RIMBORSO_DATA_RIMBORSO = "missione.rimborso.dataRimborso";
+    public final static String MISSIONE_FIELD_RIMBORSO = "missione.rimborso";
 
-	public static final String TIPOLOGIA_SPESA_FIELD_TIPO_TRATTAMENTO = "tipologiaSpesa.tipoTrattamento";
 
-	public static final String NAZIONE_FIELD_VALUE = "nazione.value";
-	public static final String NAZIONE_FIELD_ID = "nazione.id";
-	
-	public static final String MASSIMALE_FIELD_VALUE = "massimale.value";
-	public static final String MASSIMALE_FIELD_ID = "massimale.id";
-	public static final String MASSIMALE_FIELD_LIVELLO = "massimale.livello";
-	public static final String MASSIMALE_FIELD_AREA_GEOGRAFICA = "massimale.areaGeografica";
-	public static final String MASSIMALE_FIELD_TIPO = "massimale.tipo";
-	public static final String MASSIMALE_FIELD_DESCRIZIONE = "massimale.descrizione";
+    public static final String PRENOTAZIONE_FIELD_DATA_FROM = "prenotazione.dataFrom";
 
-	
-	public final static String MULTI_MATCH_ALL_FIELD = "_all";
+    public static final String VEICOLO_CNR_FIELD_STATO = "veicoloCNR.stato";
+    public static final String VEICOLO_CNR_FIELD_TARGA = "veicoloCNR.targa";
+    public static final String VEICOLO_CNR_FIELD_CARTA_CIRCOLAZIONE = "veicoloCNR.cartaCircolazione";
+    public static final String VEICOLO_CNR_FIELD_POLIZZA_ASSICURATIVA = "veicoloCNR.polizzaAssicurativa";
+    public static final String VEICOLO_CNR_FIELD_ID = "veicoloCNR.id";
+    public static final String VEICOLO_CNR_FIELD_TIPO = "veicoloCNR.tipo";
+
+
+    public static final String QUALIFICA_USER_FIELD_VALUE = "qualificaUser.value";
+    public static final String QUALIFICA_USER_FIELD_ID = "qualificaUser.id";
+
+    public static final String RIMBORSO_KM_FIELD_VALUE = "rimborsoKm.value";
+    public static final String RIMBORSO_KM_FIELD_ID = "rimborsoKm.id";
+
+
+    public static final String TIPOLOGIA_SPESA_FIELD_VALUE = "tipologiaSpesa.value";
+    public static final String TIPOLOGIA_SPESA_FIELD_ID = "tipologiaSpesa.id";
+    public static final String TIPOLOGIA_SPESA_FIELD_ESTERA = "tipologiaSpesa.estera";
+    public static final String TIPOLOGIA_SPESA_FIELD_ITALIA = "tipologiaSpesa.italia";
+
+    public static final String TIPOLOGIA_SPESA_FIELD_TIPO_TRATTAMENTO = "tipologiaSpesa.tipoTrattamento";
+
+    public static final String NAZIONE_FIELD_VALUE = "nazione.value";
+    public static final String NAZIONE_FIELD_ID = "nazione.id";
+
+    public static final String MASSIMALE_FIELD_VALUE = "massimale.value";
+    public static final String MASSIMALE_FIELD_ID = "massimale.id";
+    public static final String MASSIMALE_FIELD_LIVELLO = "massimale.livello";
+    public static final String MASSIMALE_FIELD_AREA_GEOGRAFICA = "massimale.areaGeografica";
+    public static final String MASSIMALE_FIELD_TIPO = "massimale.tipo";
+    public static final String MASSIMALE_FIELD_DESCRIZIONE = "massimale.descrizione";
+
+
+    public final static String MULTI_MATCH_ALL_FIELD = "_all";
 }
