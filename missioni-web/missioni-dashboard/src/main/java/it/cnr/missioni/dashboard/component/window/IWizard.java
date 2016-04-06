e8efdf835d605a3241e2c2b0d4b5c532c95e6499
vito.salvia@gmail.com <vito.salvia@gmail.com>
2016-04-06 12:01:35 +0200

    Builder improvements

--- missioni-web/missioni-dashboard/src/main/java/it/cnr/missioni/dashboard/component/window/IWizard.java
+++ missioni-web/missioni-dashboard/src/main/java/it/cnr/missioni/dashboard/component/window/IWizard.java
@@ -1,90 +1,112 @@
 package it.cnr.missioni.dashboard.component.window;
 
-import org.vaadin.teemu.wizards.Wizard;
-import org.vaadin.teemu.wizards.event.WizardProgressListener;
-
-import it.cnr.missioni.dashboard.event.DashboardEventBus;
 import it.cnr.missioni.dashboard.event.DashboardEvent.CloseOpenWindowsEvent;
 import it.cnr.missioni.dashboard.event.DashboardEvent.IEventResetSelectedMissione;
+import it.cnr.missioni.dashboard.event.DashboardEventBus;
 import it.cnr.missioni.model.missione.Missione;
 import it.cnr.missioni.model.user.User;
+import org.vaadin.teemu.wizards.Wizard;
+import org.vaadin.teemu.wizards.event.WizardProgressListener;
 
 /**
  * @author Salvia Vito
  */
 public interface IWizard {
-	
-	void build();
-	
-	Wizard getWizard();
-	
-	void setMissione(Missione missione);
-	
-	void setUser(User user);
-	
-	void setModifica(boolean modifica);
-	
-	void setEnabled(boolean enabled);
-	
-	void isAdmin(boolean enambled);
-	
-	void setEvent(IEventResetSelectedMissione event);
-	
-	
-	public abstract class AbstractWizard implements WizardProgressListener,  IWizard{
 
-		private Wizard wizard;
-		protected boolean enabled;
-		protected boolean modifica;
-		protected boolean isAdmin;
+    /**
+     *
+     */
+    void build();
 
-		protected void endWizard() {
-			getWizard().setVisible(false);
-			DashboardEventBus.post(new CloseOpenWindowsEvent());
-		}
-		
-		protected void buildWizard(){
-			setWizard(new Wizard());
-			getWizard().setUriFragmentEnabled(true);
-			getWizard().setVisible(true);
-			getWizard().addListener(this);
-			getWizard().getBackButton().setCaption("Indietro");
-			getWizard().getCancelButton().setCaption("Cancella");
-			getWizard().getNextButton().setCaption("Avanti");
-			getWizard().getFinishButton().setCaption("Concludi");
-			getWizard().setWidth("100%");
-			getWizard().setHeight("95%");
+    /**
+     * @return {@link Wizard}
+     */
+    Wizard getWizard();
 
-		}
+    /**
+     * @param missione
+     */
+    void setMissione(Missione missione);
 
-		/**
-		 * @return the wizard
-		 */
-		public Wizard getWizard() {
-			return wizard;
-		}
+    /**
+     * @param user
+     */
+    void setUser(User user);
 
-		/**
-		 * @param wizard 
-		 */
-		public void setWizard(Wizard wizard) {
-			this.wizard = wizard;
-		}
-		
-		public abstract void setMissione(Missione missione);
-		
-		public abstract void setUser(User user);
-		
-		public abstract void setEnabled(boolean enabled);
-		
-		public abstract void isAdmin(boolean isAdmin);
-		
-		public abstract void setEvent(IEventResetSelectedMissione event);
+    /**
+     * @param modifica
+     */
+    void setModifica(boolean modifica);
 
-		public abstract void setModifica(boolean modifica);
+    /**
+     * @param enabled
+     */
+    void setEnabled(boolean enabled);
+
+    /**
+     * @param enambled
+     */
+    void isAdmin(boolean enambled);
+
+    /***
+     * @param event
+     */
+    void setEvent(IEventResetSelectedMissione event);
 
 
-	}
+    public abstract class AbstractWizard implements WizardProgressListener, IWizard {
+
+        protected boolean enabled;
+        protected boolean modifica;
+        protected boolean isAdmin;
+        private Wizard wizard;
+
+        protected void endWizard() {
+            getWizard().setVisible(false);
+            DashboardEventBus.post(new CloseOpenWindowsEvent());
+        }
+
+        protected void buildWizard() {
+            setWizard(new Wizard());
+            getWizard().setUriFragmentEnabled(true);
+            getWizard().setVisible(true);
+            getWizard().addListener(this);
+            getWizard().getBackButton().setCaption("Indietro");
+            getWizard().getCancelButton().setCaption("Cancella");
+            getWizard().getNextButton().setCaption("Avanti");
+            getWizard().getFinishButton().setCaption("Concludi");
+            getWizard().setWidth("100%");
+            getWizard().setHeight("95%");
+        }
+
+        /**
+         * @return the wizard
+         */
+        public Wizard getWizard() {
+            return wizard;
+        }
+
+        /**
+         * @param wizard
+         */
+        public void setWizard(Wizard wizard) {
+            this.wizard = wizard;
+        }
+
+        public abstract void setMissione(Missione missione);
+
+        public abstract void setUser(User user);
+
+        public abstract void setEnabled(boolean enabled);
+
+        public abstract void isAdmin(boolean isAdmin);
+
+        public abstract void setEvent(IEventResetSelectedMissione event);
+
+        public abstract void setModifica(boolean modifica);
+
+
+    }
 
 
 }
