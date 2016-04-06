46e9681afd5cc422c5215cd4c9bbc0465a04fd8d
vito.salvia@gmail.com <vito.salvia@gmail.com>
2016-04-06 12:25:21 +0200

    Refactoring and calling form improvements

--- missioni-web/missioni-dashboard/src/main/java/it/cnr/missioni/dashboard/component/window/admin/NazioneWindow.java
+++ missioni-web/missioni-dashboard/src/main/java/it/cnr/missioni/dashboard/component/window/admin/NazioneWindow.java
@@ -3,85 +3,82 @@ package it.cnr.missioni.dashboard.component.window.admin;
 import com.vaadin.data.Validator.InvalidValueException;
 import com.vaadin.data.fieldgroup.FieldGroup.CommitException;
 import com.vaadin.server.FontAwesome;
-import com.vaadin.ui.*;
+import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Notification.Type;
+import com.vaadin.ui.UI;
 import com.vaadin.ui.themes.ValoTheme;
 import it.cnr.missioni.dashboard.action.admin.NazioneAction;
-import it.cnr.missioni.dashboard.component.form.nazione.NazioneForm;
+import it.cnr.missioni.dashboard.component.form.nazione.INazioneForm;
 import it.cnr.missioni.dashboard.component.window.IWindow;
-import it.cnr.missioni.dashboard.event.DashboardEvent.CloseOpenWindowsEvent;
 import it.cnr.missioni.dashboard.event.DashboardEventBus;
 import it.cnr.missioni.dashboard.utility.Utility;
 import it.cnr.missioni.model.configuration.Nazione;
 
-public class NazioneWindow extends IWindow.AbstractWindow {
+public class NazioneWindow extends IWindow.AbstractWindow<Nazione, NazioneWindow> {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -6599225757081293003L;
+    public static final String ID = "nazionewindow";
+    /**
+     *
+     */
+    private static final long serialVersionUID = -6599225757081293003L;
+    private INazioneForm nazioneForm;
 
-	public static final String ID = "nazionewindow";
+    protected NazioneWindow() {
+    }
 
-	private NazioneForm nazioneForm;
+    public static NazioneWindow getNazioneWindow() {
+        return new NazioneWindow();
+    }
 
-	private  Nazione nazione;
+    public NazioneWindow build() {
+        setId(ID);
+        buildWindow();
+        detailsWrapper.addComponent(buildTab());
+        content.addComponent(buildFooter());
+        UI.getCurrent().addWindow(this);
+        this.focus();
+        return self();
+    }
 
-	private NazioneWindow(final Nazione nazione,final boolean isAdmin,final boolean enabled, final boolean modifica) {
-
-		super(isAdmin,enabled,modifica);
-		this.nazione = nazione;
-		setId(ID);
-		build();
-		detailsWrapper.addComponent(buildTab());
-		content.addComponent(buildFooter());
-
-	}
-
-    protected HorizontalLayout buildTab(){
-        this.nazioneForm = new NazioneForm(nazione,true,true,modifica);
-        HorizontalLayout tab = super.buildTab("Nazione",FontAwesome.GLOBE,this.nazioneForm);
+    protected HorizontalLayout buildTab() {
+//        this.nazioneForm = new NazioneForm(nazione,true,true,modifica);
+        this.nazioneForm = INazioneForm.NazioneForm.getNazioneForm().withBean(bean).withIsAdmin(isAdmin).withEnabled(enabled).withModifica(modifica).build();
+        HorizontalLayout tab = super.buildTab("Nazione", FontAwesome.GLOBE, this.nazioneForm);
         return tab;
     }
-	
-	protected Component buildFooter() {
-		ok.addStyleName(ValoTheme.BUTTON_PRIMARY);
-		ok.addClickListener(new ClickListener() {
-			/**
-			 * 
-			 */
-			private static final long serialVersionUID = 6503857843364882408L;
 
-			@Override
-			public void buttonClick(ClickEvent event) {
+    protected Component buildFooter() {
+        ok.addStyleName(ValoTheme.BUTTON_PRIMARY);
+        ok.addClickListener(new ClickListener() {
+            /**
+             *
+             */
+            private static final long serialVersionUID = 6503857843364882408L;
 
-				try {
+            @Override
+            public void buttonClick(ClickEvent event) {
+                try {
+                    bean = nazioneForm.validate();
+                    DashboardEventBus.post(new NazioneAction(bean, modifica));
+                    close();
+                } catch (InvalidValueException | CommitException e) {
+                    Utility.getNotification(Utility.getMessage("error_message"), Utility.getMessage("commit_failed"),
+                            Type.ERROR_MESSAGE);
+                }
+            }
+        });
+        ok.focus();
+        footer.addComponent(ok);
+        footer.setComponentAlignment(ok, Alignment.TOP_RIGHT);
+        return footer;
+    }
 
-					nazione = nazioneForm.validate();
-
-					DashboardEventBus.post(new NazioneAction(nazione, modifica));
-					close();
-
-				} catch (InvalidValueException | CommitException e) {
-					Utility.getNotification(Utility.getMessage("error_message"), Utility.getMessage("commit_failed"),
-							Type.ERROR_MESSAGE);
-				}
-
-			}
-		});
-		ok.focus();
-		footer.addComponent(ok);
-		footer.setComponentAlignment(ok, Alignment.TOP_RIGHT);
-		return footer;
-	}
-
-	public static void open(final Nazione nazione,final boolean isAdmin,final boolean enabled,final  boolean modifica) {
-		DashboardEventBus.post(new CloseOpenWindowsEvent());
-		Window w = new NazioneWindow(nazione,isAdmin,enabled, modifica);
-		UI.getCurrent().addWindow(w);
-		w.focus();
-	}
+    public NazioneWindow self() {
+        return this;
+    }
 
 }
