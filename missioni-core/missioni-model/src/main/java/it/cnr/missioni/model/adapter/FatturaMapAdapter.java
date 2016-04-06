c044a0bbf2eab02a939143a850fff6dacaa0dd8d
vito.salvia@gmail.com <vito.salvia@gmail.com>
2016-04-06 12:25:59 +0200

    Refactoring

--- missioni-core/missioni-model/src/main/java/it/cnr/missioni/model/adapter/FatturaMapAdapter.java
+++ missioni-core/missioni-model/src/main/java/it/cnr/missioni/model/adapter/FatturaMapAdapter.java
@@ -1,16 +1,15 @@
 package it.cnr.missioni.model.adapter;
 
-import java.util.Map;
-
+import it.cnr.missioni.model.rimborso.Fattura;
 import org.geosdi.geoplatform.response.collection.GenericMapAdapter;
 import org.geosdi.geoplatform.response.collection.GenericMapType;
 
-import it.cnr.missioni.model.rimborso.Fattura;
+import java.util.Map;
 
 /**
  * @author Salvia Vito
  */
-public class FatturaMapAdapter extends GenericMapAdapter<String, Fattura>{
+public class FatturaMapAdapter extends GenericMapAdapter<String, Fattura> {
 
     @Override
     public Map<String, Fattura> unmarshal(GenericMapType<String, Fattura> f) throws Exception {
