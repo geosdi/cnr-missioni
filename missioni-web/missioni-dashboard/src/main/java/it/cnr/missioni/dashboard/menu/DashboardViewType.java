c96e644e2de5757eb5acd14b406b79594a5c7a62
vito.salvia@gmail.com <vito.salvia@gmail.com>
2016-04-06 11:36:51 +0200

    Add new field to enum

--- missioni-web/missioni-dashboard/src/main/java/it/cnr/missioni/dashboard/menu/DashboardViewType.java
+++ missioni-web/missioni-dashboard/src/main/java/it/cnr/missioni/dashboard/menu/DashboardViewType.java
@@ -1,14 +1,18 @@
 package it.cnr.missioni.dashboard.menu;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.vaadin.navigator.View;
 import com.vaadin.server.FontAwesome;
 import com.vaadin.server.Resource;
 
-import it.cnr.missioni.dashboard.view.PrenotazioneVeicoloCNRView;
+import it.cnr.missioni.dashboard.view.GestioneMissioneView;
+import it.cnr.missioni.dashboard.view.GestioneRimborsoView;
+import it.cnr.missioni.dashboard.view.GestioneVeicoloView;
 import it.cnr.missioni.dashboard.view.HomeView;
+import it.cnr.missioni.dashboard.view.PrenotazioneVeicoloCNRView;
 import it.cnr.missioni.dashboard.view.admin.AdminHomeView;
 import it.cnr.missioni.dashboard.view.admin.GestioneMassimaleView;
 import it.cnr.missioni.dashboard.view.admin.GestioneMissioneAdminView;
@@ -19,45 +23,61 @@ import it.cnr.missioni.dashboard.view.admin.GestioneRimborsoKmView;
 import it.cnr.missioni.dashboard.view.admin.GestioneTipologiaSpesaView;
 import it.cnr.missioni.dashboard.view.admin.GestioneUserAdminView;
 import it.cnr.missioni.dashboard.view.admin.GestioneVeicoloCNRView;
-import it.cnr.missioni.dashboard.view.GestioneMissioneView;
-import it.cnr.missioni.dashboard.view.GestioneRimborsoView;
-import it.cnr.missioni.dashboard.view.GestioneVeicoloView;
 
 
 public enum DashboardViewType {
-    HOME_ADMIN("home admin",AdminHomeView.class, FontAwesome.HOME, true),
-
-    HOME("home", HomeView.class, FontAwesome.HOME, true) ,
-    COMPLETA_REGISTRAZIONE("completa registrazione",null, FontAwesome.USER, true),
-    GESTIONE_MISSIONE("gestione missione",GestioneMissioneView.class, FontAwesome.SUITCASE, true),
-    GESTIONE_RIMBORSO("gestione rimborso",GestioneRimborsoView.class, FontAwesome.EURO, true),
-    GESTIONE_VEICOLO_PROPRIO("gestione veicolo proprio",GestioneVeicoloView.class, FontAwesome.CAR, true),
-    CALENDARIO("prenotazione veicolo CNR", PrenotazioneVeicoloCNRView.class, FontAwesome.CALENDAR, true),
-    
-    GESTIONE_USER_ADMIN("gestione user",GestioneUserAdminView.class, FontAwesome.USERS, true),
-    GESTIONE_MISSIONE_ADMIN("gestione missione admin",GestioneMissioneAdminView.class, FontAwesome.SUITCASE, true),
-    GESTIONE_RIMBORSO_ADMIN("gestione rimborso admin",GestioneRimborsoAdminView.class, FontAwesome.EURO, true),
-    GESTIONE_VEICOLO_CNR_ADMIN("gestione veicolo CNR",GestioneVeicoloCNRView.class, FontAwesome.CAR, true),
-    GESTIONE_QUALIFICA_USER_ADMIN("gestione qualifica user",GestioneQualificaUserView.class, FontAwesome.GRADUATION_CAP, true),
-    GESTIONE_NAZIONE_ADMIN("gestione nazione",GestioneNazioneView.class, FontAwesome.GLOBE, true),
-    GESTIONE_RIMBORSO_KM_ADMIN("gestione rimborso km",GestioneRimborsoKmView.class, FontAwesome.EURO, true),
-    GESTIONE_TIPOLOGIA_SPESA_ADMIN("gestione tipologia spesa",GestioneTipologiaSpesaView.class, FontAwesome.LIST, true),
-    GESTIONE_MASSIMALE_ADMIN("gestione massimale",GestioneMassimaleView.class, FontAwesome.EURO, true),
-
-    ;
+	//USER MENU
+    HOME_ADMIN("home admin", AdminHomeView.class, FontAwesome.HOME, true,false),
+    HOME("home", HomeView.class, FontAwesome.HOME, true,false),
+    COMPLETA_REGISTRAZIONE("completa registrazione", null, FontAwesome.USER, true,false),
+    GESTIONE_MISSIONE("gestione missione", GestioneMissioneView.class, FontAwesome.SUITCASE, true,false),
+    GESTIONE_RIMBORSO("gestione rimborso", GestioneRimborsoView.class, FontAwesome.EURO, true,false),
+    GESTIONE_VEICOLO_PROPRIO("gestione veicolo proprio", GestioneVeicoloView.class, FontAwesome.CAR, true,false),
+    CALENDARIO("prenotazione veicolo CNR", PrenotazioneVeicoloCNRView.class, FontAwesome.CALENDAR, true,false),
+    //ADMIN MENU
+    GESTIONE_USER_ADMIN("gestione user", GestioneUserAdminView.class, FontAwesome.USERS, true,true),
+    GESTIONE_MISSIONE_ADMIN("gestione missione admin", GestioneMissioneAdminView.class, FontAwesome.SUITCASE, true,true),
+    GESTIONE_RIMBORSO_ADMIN("gestione rimborso admin", GestioneRimborsoAdminView.class, FontAwesome.EURO, true,true),
+    GESTIONE_VEICOLO_CNR_ADMIN("gestione veicolo CNR", GestioneVeicoloCNRView.class, FontAwesome.CAR, true,true),
+    GESTIONE_QUALIFICA_USER_ADMIN("gestione qualifica user", GestioneQualificaUserView.class, FontAwesome.GRADUATION_CAP, true,true),
+    GESTIONE_NAZIONE_ADMIN("gestione nazione", GestioneNazioneView.class, FontAwesome.GLOBE, true,true),
+    GESTIONE_RIMBORSO_KM_ADMIN("gestione rimborso km", GestioneRimborsoKmView.class, FontAwesome.EURO, true,true),
+    GESTIONE_TIPOLOGIA_SPESA_ADMIN("gestione tipologia spesa", GestioneTipologiaSpesaView.class, FontAwesome.LIST, true,true),
+    GESTIONE_MASSIMALE_ADMIN("gestione massimale", GestioneMassimaleView.class, FontAwesome.EURO, true,true);
 
     private final String viewName;
     private final Class<? extends View> viewClass;
     private final Resource icon;
     private final boolean stateful;
+    private final boolean isAdmin;
 
     private DashboardViewType(final String viewName,
             final Class<? extends View> viewClass, final Resource icon,
-            final boolean stateful) {
+            final boolean stateful,final boolean isAdmin) {
         this.viewName = viewName;
         this.viewClass = viewClass;
         this.icon = icon;
         this.stateful = stateful;
+        this.isAdmin = isAdmin;
+    }
+
+    public static List<DashboardViewType> getMenuUser() {
+    	return Arrays.asList(DashboardViewType.values()).stream().filter(f -> !f.isAdmin).collect(Collectors.toList());
+    }
+
+    public static List<DashboardViewType> getMenuAdmin() {
+    	return Arrays.asList(DashboardViewType.values()).stream().filter(f -> f.isAdmin).collect(Collectors.toList());
+    }
+
+    public static DashboardViewType getByViewName(final String viewName) {
+        DashboardViewType result = null;
+        for (DashboardViewType viewType : values()) {
+            if (viewType.getViewName().equals(viewName)) {
+                result = viewType;
+                break;
+            }
+        }
+        return result;
     }
 
     public boolean isStateful() {
@@ -74,44 +94,6 @@ public enum DashboardViewType {
 
     public Resource getIcon() {
         return icon;
-    }
-    
-    public static List<DashboardViewType> getMenuUser(){
-    	List<DashboardViewType> menuList = new ArrayList<DashboardViewType>();
-    	menuList.add(HOME);
-    	menuList.add(COMPLETA_REGISTRAZIONE);
-    	menuList.add(GESTIONE_MISSIONE);
-    	menuList.add(GESTIONE_RIMBORSO);
-    	menuList.add(GESTIONE_VEICOLO_PROPRIO);
-    	menuList.add(CALENDARIO);
-    	return menuList;
-    }
-    
-    public static List<DashboardViewType> getMenuAdmin(){
-    	List<DashboardViewType> menuList = new ArrayList<DashboardViewType>();
-    	menuList.add(HOME_ADMIN);
-    	menuList.add(GESTIONE_USER_ADMIN);
-    	menuList.add(GESTIONE_MISSIONE_ADMIN);
-    	menuList.add(GESTIONE_RIMBORSO_ADMIN);
-    	menuList.add(GESTIONE_VEICOLO_CNR_ADMIN);
-    	menuList.add(GESTIONE_QUALIFICA_USER_ADMIN);
-    	menuList.add(GESTIONE_NAZIONE_ADMIN);
-    	menuList.add(GESTIONE_RIMBORSO_KM_ADMIN);
-    	menuList.add(GESTIONE_TIPOLOGIA_SPESA_ADMIN);
-    	menuList.add(GESTIONE_QUALIFICA_USER_ADMIN);
-    	menuList.add(GESTIONE_MASSIMALE_ADMIN);
-    	return menuList;
-    }
-
-    public static DashboardViewType getByViewName(final String viewName) {
-    	DashboardViewType result = null;
-        for (DashboardViewType viewType : values()) {
-            if (viewType.getViewName().equals(viewName)) {
-                result = viewType;
-                break;
-            }
-        }
-        return result;
     }
 
 }
