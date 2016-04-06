8fe705c9b0f3d02501c9e2413e3a5ac7392f32fb
vito.salvia@gmail.com <vito.salvia@gmail.com>
2016-04-06 11:14:33 +0200

    Add builder to window

--- missioni-web/missioni-dashboard/src/main/java/it/cnr/missioni/dashboard/component/window/VeicoloWindow.java
+++ missioni-web/missioni-dashboard/src/main/java/it/cnr/missioni/dashboard/component/window/VeicoloWindow.java
@@ -4,89 +4,80 @@ import com.vaadin.data.Validator.InvalidValueException;
 import com.vaadin.data.fieldgroup.FieldGroup.CommitException;
 import com.vaadin.server.FontAwesome;
 import com.vaadin.ui.Alignment;
-import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.Component;
-import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Notification.Type;
 import com.vaadin.ui.UI;
-import com.vaadin.ui.Window;
 import com.vaadin.ui.themes.ValoTheme;
-
 import it.cnr.missioni.dashboard.action.VeicoloAction;
-import it.cnr.missioni.dashboard.component.form.veicolo.VeicoloForm;
-import it.cnr.missioni.dashboard.event.DashboardEvent.CloseOpenWindowsEvent;
+import it.cnr.missioni.dashboard.component.form.veicolo.IVeicoloForm;
 import it.cnr.missioni.dashboard.event.DashboardEventBus;
 import it.cnr.missioni.dashboard.utility.Utility;
 import it.cnr.missioni.model.user.Veicolo;
 
-public class VeicoloWindow  extends  IWindow.AbstractWindow  {
+public class VeicoloWindow extends IWindow.AbstractWindow<Veicolo, VeicoloWindow> {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -333945068835078811L;
+    public static final String ID = "veicolowindow";
+    /**
+     *
+     */
+    private static final long serialVersionUID = -333945068835078811L;
+    private IVeicoloForm veicoloForm;
 
-	public static final String ID = "veicolowindow";
+    protected VeicoloWindow() {
+    }
 
+    public static VeicoloWindow getVeicoloWindow() {
+        return new VeicoloWindow();
+    }
 
-	private Veicolo veicolo;
-	private VeicoloForm veicoloForm;
-
-
-	private VeicoloWindow(final Veicolo veicolo, final boolean isAdmin,final boolean enabled, final boolean modifica) {
-		super(isAdmin,enabled,modifica);
-		this.veicolo = veicolo;
-		setId(ID);
-		build();
-		buildTab();
-		content.addComponent(buildFooter());
-
-	}
-	
-	private void buildTab() {
-		this.veicoloForm = new VeicoloForm(veicolo, false,true,modifica);
-		detailsWrapper.addComponent(buildTab("Veicolo", FontAwesome.CAR, this.veicoloForm ));
-	}
+    public VeicoloWindow build() {
+        setId(ID);
+        buildWindow();
+        buildTab();
+        content.addComponent(buildFooter());
+        UI.getCurrent().addWindow(this);
+        this.focus();
+        return self();
+    }
 
 
-	protected Component buildFooter() {
-		ok.addStyleName(ValoTheme.BUTTON_PRIMARY);
-		ok.addClickListener(new ClickListener() {
-			/**
-			 * 
-			 */
-			private static final long serialVersionUID = -932339804718579357L;
+    private void buildTab() {
+//		this.veicoloForm = new VeicoloForm(veicolo, false,true,modifica);
+        this.veicoloForm = IVeicoloForm.VeicoloForm.getVeicoloForm().withBean(bean).withIsAdmin(isAdmin).withEnabled(enabled).withModifica(modifica).build();
+        detailsWrapper.addComponent(buildTab("Veicolo", FontAwesome.CAR, this.veicoloForm));
+    }
 
-			@Override
-			public void buttonClick(ClickEvent event) {
 
-				try {
+    protected Component buildFooter() {
+        ok.addStyleName(ValoTheme.BUTTON_PRIMARY);
+        ok.addClickListener(new ClickListener() {
+            /**
+             *
+             */
+            private static final long serialVersionUID = -932339804718579357L;
 
-					Veicolo new_veicolo = veicoloForm.validate();
+            @Override
+            public void buttonClick(ClickEvent event) {
+                try {
+                    Veicolo new_veicolo = veicoloForm.validate();
+                    DashboardEventBus.post(new VeicoloAction(new_veicolo, modifica));
+                    close();
+                } catch (InvalidValueException | CommitException e) {
+                    Utility.getNotification(Utility.getMessage("error_message"), Utility.getMessage("commit_failed"),
+                            Type.ERROR_MESSAGE);
+                }
+            }
+        });
+        ok.focus();
+        footer.addComponent(ok);
+        footer.setComponentAlignment(ok, Alignment.TOP_RIGHT);
+        return footer;
+    }
 
-					DashboardEventBus.post(new VeicoloAction(new_veicolo,modifica));
-					close();
-
-				} catch (InvalidValueException | CommitException e) {
-					Utility.getNotification(Utility.getMessage("error_message"), Utility.getMessage("commit_failed"),
-							Type.ERROR_MESSAGE);
-				}
-
-			}
-		});
-		ok.focus();
-		footer.addComponent(ok);
-		footer.setComponentAlignment(ok, Alignment.TOP_RIGHT);
-		return footer;
-	}
-
-	public static void open(final Veicolo veicolo,final boolean isAdmin,final boolean enabled, boolean modifica) {
-		DashboardEventBus.post(new CloseOpenWindowsEvent());
-		Window w = new VeicoloWindow(veicolo,isAdmin,enabled, modifica);
-		UI.getCurrent().addWindow(w);
-		w.focus();
-	}
+    public VeicoloWindow self() {
+        return this;
+    }
 
 }
--- missioni-web/missioni-dashboard/src/main/java/it/cnr/missioni/dashboard/component/window/admin/VeicoloCNRWindow.java
+++ missioni-web/missioni-dashboard/src/main/java/it/cnr/missioni/dashboard/component/window/admin/VeicoloCNRWindow.java
@@ -4,87 +4,79 @@ import com.vaadin.data.Validator.InvalidValueException;
 import com.vaadin.data.fieldgroup.FieldGroup.CommitException;
 import com.vaadin.server.FontAwesome;
 import com.vaadin.ui.Alignment;
-import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.Component;
-import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Notification.Type;
 import com.vaadin.ui.UI;
-import com.vaadin.ui.Window;
 import com.vaadin.ui.themes.ValoTheme;
-
 import it.cnr.missioni.dashboard.action.admin.VeicoloCNRAction;
-import it.cnr.missioni.dashboard.component.form.veicolocnr.VeicoloCNRForm;
+import it.cnr.missioni.dashboard.component.form.veicolocnr.IVeicoloCNRForm;
 import it.cnr.missioni.dashboard.component.window.IWindow;
-import it.cnr.missioni.dashboard.event.DashboardEvent.CloseOpenWindowsEvent;
 import it.cnr.missioni.dashboard.event.DashboardEventBus;
 import it.cnr.missioni.dashboard.utility.Utility;
 import it.cnr.missioni.model.prenotazione.VeicoloCNR;
 
-public class VeicoloCNRWindow extends IWindow.AbstractWindow {
+public class VeicoloCNRWindow extends IWindow.AbstractWindow<VeicoloCNR, VeicoloCNRWindow> {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -2904991157664896304L;
+    public static final String ID = "veicoloCNRwindow";
+    /**
+     *
+     */
+    private static final long serialVersionUID = -2904991157664896304L;
+    private IVeicoloCNRForm veicoloCNRForm;
 
-	public static final String ID = "veicoloCNRwindow";
+    protected VeicoloCNRWindow() {
+    }
 
+    public static VeicoloCNRWindow getVeicoloCNRWindow() {
+        return new VeicoloCNRWindow();
+    }
 
-	private VeicoloCNRForm veicoloCNRForm;
-	private final VeicoloCNR veicoloCNR;
+    public VeicoloCNRWindow build() {
+        setId(ID);
+        buildWindow();
+        buildTab();
+        content.addComponent(buildFooter());
+        UI.getCurrent().addWindow(this);
+        this.focus();
+        return self();
+    }
 
-	private VeicoloCNRWindow(final VeicoloCNR veicoloCNR,final boolean isAdmin,final boolean enabled,final boolean modifica) {
-		super(isAdmin,enabled,modifica);
-		this.veicoloCNR = veicoloCNR;
-		setId(ID);
-		build();
-		buildTab();
-		content.addComponent(buildFooter());
+    private void buildTab() {
+//		this.veicoloCNRForm = new VeicoloCNRForm(veicoloCNR, true,true,modifica);
+        this.veicoloCNRForm = IVeicoloCNRForm.VeicoloCNRForm.getVeicoloCNRForm().withBean(bean).withIsAdmin(isAdmin).withEnabled(enabled).withModifica(modifica).build();
+        detailsWrapper.addComponent(buildTab("Veicolo CNR", FontAwesome.CAR, this.veicoloCNRForm));
+    }
 
-	}
+    protected Component buildFooter() {
+        ok.addStyleName(ValoTheme.BUTTON_PRIMARY);
+        ok.addClickListener(new ClickListener() {
+            /**
+             *
+             */
+            private static final long serialVersionUID = -1658601658889451200L;
 
-	private void buildTab() {
-		this.veicoloCNRForm = new VeicoloCNRForm(veicoloCNR, true,true,modifica);
-		detailsWrapper.addComponent(buildTab("Veicolo CNR", FontAwesome.CAR, this.veicoloCNRForm ));
-	}
+            @Override
+            public void buttonClick(ClickEvent event) {
+                try {
+                    VeicoloCNR new_veicoloCNR = veicoloCNRForm.validate();
+                    DashboardEventBus.post(new VeicoloCNRAction(new_veicoloCNR, modifica));
+                    close();
+                } catch (InvalidValueException | CommitException e) {
+                    Utility.getNotification(Utility.getMessage("error_message"), Utility.getMessage("commit_failed"),
+                            Type.ERROR_MESSAGE);
+                }
+            }
+        });
+        ok.focus();
+        footer.addComponent(ok);
+        footer.setComponentAlignment(ok, Alignment.TOP_RIGHT);
+        return footer;
+    }
 
-	protected Component buildFooter() {
-		ok.addStyleName(ValoTheme.BUTTON_PRIMARY);
-		ok.addClickListener(new ClickListener() {
-			/**
-			 * 
-			 */
-			private static final long serialVersionUID = -1658601658889451200L;
-
-			@Override
-			public void buttonClick(ClickEvent event) {
-
-				try {
-					VeicoloCNR new_veicoloCNR = veicoloCNRForm.validate();
-
-					DashboardEventBus.post(new VeicoloCNRAction(new_veicoloCNR, modifica));
-					close();
-
-				} catch (InvalidValueException | CommitException e) {
-					Utility.getNotification(Utility.getMessage("error_message"), Utility.getMessage("commit_failed"),
-							Type.ERROR_MESSAGE);
-				}
-
-			}
-		});
-		ok.focus();
-		footer.addComponent(ok);
-		footer.setComponentAlignment(ok, Alignment.TOP_RIGHT);
-		return footer;
-	}
-
-	public static void open(final VeicoloCNR veicoloCNR,final boolean isAdmin,final boolean enabled,final boolean modifica) {
-		DashboardEventBus.post(new CloseOpenWindowsEvent());
-		Window w = new VeicoloCNRWindow(veicoloCNR,isAdmin,enabled, modifica);
-		UI.getCurrent().addWindow(w);
-		w.focus();
-	}
+    public VeicoloCNRWindow self() {
+        return this;
+    }
 
 }
