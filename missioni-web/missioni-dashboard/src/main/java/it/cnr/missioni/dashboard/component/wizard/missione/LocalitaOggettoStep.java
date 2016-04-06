31f4ad5b94f40a9ee0727a85a1d2ed624e9718b9
vito.salvia@gmail.com <vito.salvia@gmail.com>
2016-04-06 12:01:04 +0200

    refactoring and calling window improvements

--- missioni-web/missioni-dashboard/src/main/java/it/cnr/missioni/dashboard/component/wizard/missione/LocalitaOggettoStep.java
+++ missioni-web/missioni-dashboard/src/main/java/it/cnr/missioni/dashboard/component/wizard/missione/LocalitaOggettoStep.java
@@ -1,52 +1,50 @@
 package it.cnr.missioni.dashboard.component.wizard.missione;
 
-import org.vaadin.teemu.wizards.WizardStep;
-
 import com.vaadin.data.Validator.InvalidValueException;
 import com.vaadin.data.fieldgroup.FieldGroup.CommitException;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.Notification.Type;
-
-import it.cnr.missioni.dashboard.component.form.missione.LocalitaOggettoMissioneForm;
+import it.cnr.missioni.dashboard.component.form.missione.ILocalitaOggettoMissioneForm;
 import it.cnr.missioni.dashboard.utility.Utility;
 import it.cnr.missioni.model.missione.Missione;
+import org.vaadin.teemu.wizards.WizardStep;
 
 /**
  * @author Salvia Vito
  */
 public class LocalitaOggettoStep implements WizardStep {
 
-	private Missione missione;
-	private LocalitaOggettoMissioneForm localitaOggettoMissioneForm;
+    private Missione missione;
+    private ILocalitaOggettoMissioneForm localitaOggettoMissioneForm;
 
-	public String getCaption() {
-		return "Step 2";
-	}
+    public LocalitaOggettoStep(Missione missione, boolean isAdmin, boolean enabled, boolean modifica) {
+        this.missione = missione;
+//		this.localitaOggettoMissioneForm = new LocalitaOggettoMissioneForm(missione, isAdmin, enabled,modifica);
+        this.localitaOggettoMissioneForm = ILocalitaOggettoMissioneForm.LocalitaOggettoMissioneForm.getDatiPeriodoMissioneForm().withBean(missione).withIsAdmin(isAdmin).withEnabled(enabled).withModifica(modifica).build();
+    }
 
-	public LocalitaOggettoStep(Missione missione,boolean isAdmin,boolean enabled,boolean modifica) {
-		this.missione = missione;
-		this.localitaOggettoMissioneForm = new LocalitaOggettoMissioneForm(missione, isAdmin, enabled,modifica);
+    public String getCaption() {
+        return "Step 2";
+    }
 
-	}
+    public Component getContent() {
+        this.localitaOggettoMissioneForm.withIsVisible(missione.isMissioneEstera());
+        return this.localitaOggettoMissioneForm;
+    }
 
-	public Component getContent() {
-		this.localitaOggettoMissioneForm.setVisibleField(missione.isMissioneEstera());
-		return this.localitaOggettoMissioneForm;
-	}
+    public boolean onAdvance() {
+        try {
+            missione = localitaOggettoMissioneForm.validate();
+            return true;
+        } catch (CommitException | InvalidValueException e) {
+            Utility.getNotification(Utility.getMessage("error_message"), Utility.getMessage("commit_failed"),
+                    Type.ERROR_MESSAGE);
+            return false;
+        }
+    }
 
-	public boolean onAdvance() {
-		try {
-			missione = localitaOggettoMissioneForm.validate();
-			return true;
-		} catch (CommitException | InvalidValueException e) {
-			Utility.getNotification(Utility.getMessage("error_message"), Utility.getMessage("commit_failed"),
-					Type.ERROR_MESSAGE);
-			return false;
-		}
-	}
-
-	public boolean onBack() {
-		return true;
-	}
+    public boolean onBack() {
+        return true;
+    }
 
 }
