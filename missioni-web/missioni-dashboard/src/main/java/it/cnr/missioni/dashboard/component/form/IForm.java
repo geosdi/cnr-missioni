5de2e0b2bcd7f8a9968bb3939c7a3d7eba14c11b
vito.salvia@gmail.com <vito.salvia@gmail.com>
2016-04-06 11:27:41 +0200

    Create form builder

--- missioni-web/missioni-dashboard/src/main/java/it/cnr/missioni/dashboard/component/form/IForm.java
+++ missioni-web/missioni-dashboard/src/main/java/it/cnr/missioni/dashboard/component/form/IForm.java
@@ -5,79 +5,144 @@ import com.vaadin.data.fieldgroup.BeanFieldGroup;
 import com.vaadin.data.fieldgroup.FieldGroup.CommitException;
 import com.vaadin.data.fieldgroup.FieldGroupFieldFactory;
 import com.vaadin.ui.FormLayout;
+import com.vaadin.ui.Layout;
 import com.vaadin.ui.themes.ValoTheme;
-
 import it.cnr.missioni.dashboard.event.DashboardEventBus;
 import it.cnr.missioni.dashboard.utility.BeanFieldGrouFactory;
 
 /**
  * @author Salvia Vito
  */
-public interface IForm<T extends Object> {
+public interface IForm<T extends Object, B> extends Layout {
 
-	T validate()  throws CommitException,InvalidValueException ;
+    T validate() throws CommitException, InvalidValueException;
 
-	void buildFieldGroup();
+    /**
+     * @param modifica
+     * @return {@link IForm<T> }
+     */
+    B withModifica(boolean modifica);
 
-	void buildTab();
+    /**
+     * @param enabled
+     * @return {@link IForm<T> }
+     */
+    B withEnabled(boolean enabled);
 
-	void addValidator();
-	
-	void addListener();
+    /**
+     * @param isAdmin
+     * @return {@link IForm<T> }
+     */
+    B withIsAdmin(boolean isAdmin);
 
-	public abstract class FormAbstract<T> extends FormLayout implements IForm<T> {
+    /**
+     * @param bean
+     * @return {@link IForm<T> }
+     */
+    B withBean(T bean);
 
-		/**
-		* 
-		*/
-		private static final long serialVersionUID = -7214832545147375718L;
-		private BeanFieldGroup<T> fieldGroup;
-		protected T bean;
-		protected boolean enabled;
-		protected boolean modifica;
-		protected boolean isAdmin;
+    /**
+     * @return {@link IForm<T> }
+     */
+    B build();
 
-		protected FormAbstract(){}
-		
-		protected FormAbstract(T bean,boolean isAdmin,boolean enabled,boolean modifica){
-			this.enabled = enabled;
-			this.isAdmin = isAdmin;
-			this.modifica = modifica;
-			this.bean = bean;
-			DashboardEventBus.register(this);
-			addStyleName(ValoTheme.FORMLAYOUT_LIGHT);
+    /**
+     * @return {@link BeanFieldGroup<T> }
+     */
+    BeanFieldGroup<T> getFieldGroup();
 
-		}
 
-		public void buildFieldGroup() {
-					
-			getFieldGroup().setItemDataSource(bean);
-			getFieldGroup().setBuffered(true);
-			getFieldGroup().setReadOnly(!enabled);
-			FieldGroupFieldFactory fieldFactory = new BeanFieldGrouFactory();
-			getFieldGroup().setFieldFactory(fieldFactory);
-		}
-		
-		
-		public abstract void addValidator();
-		
-		public abstract void addListener();
+    void buildFieldGroup();
 
-		/**
-		 * @return the fieldGroup
-		 */
-		public BeanFieldGroup<T> getFieldGroup() {
-			return fieldGroup;
-		}
+    void buildTab();
 
-		/**
-		 * @param fieldGroup 
-		 */
-		public void setFieldGroup(BeanFieldGroup<T> fieldGroup) {
-			this.fieldGroup = fieldGroup;
-		}
+    void addValidator();
 
-	}
+    void addListener();
+
+    public abstract class FormAbstract<T, B> extends FormLayout implements IForm<T, B> {
+
+        /**
+         *
+         */
+        private static final long serialVersionUID = -7214832545147375718L;
+        protected T bean;
+        protected boolean enabled;
+        protected boolean modifica;
+        protected boolean isAdmin;
+        private BeanFieldGroup<T> fieldGroup;
+
+        protected FormAbstract() {
+            DashboardEventBus.register(this);
+            addStyleName(ValoTheme.FORMLAYOUT_LIGHT);
+        }
+
+        /**
+         * @param modifica
+         * @return {@link IForm<T> }
+         */
+        public B withModifica(boolean modifica) {
+            this.modifica = modifica;
+            return self();
+        }
+
+        /**
+         * @param enabled
+         * @return {@link IForm<T> }
+         */
+        public B withEnabled(boolean enabled) {
+            this.enabled = enabled;
+            return self();
+        }
+
+        /**
+         * @param isAdmin
+         * @return {@link IForm<T> }
+         */
+        public B withIsAdmin(boolean isAdmin) {
+            this.isAdmin = isAdmin;
+            return self();
+        }
+
+        /**
+         * @param bean
+         * @return {@link IForm<T> }
+         */
+        public B withBean(T bean) {
+            this.bean = bean;
+            return self();
+        }
+
+        public void buildFieldGroup() {
+            getFieldGroup().setItemDataSource(bean);
+            getFieldGroup().setBuffered(true);
+            getFieldGroup().setReadOnly(!enabled);
+            FieldGroupFieldFactory fieldFactory = new BeanFieldGrouFactory();
+            getFieldGroup().setFieldFactory(fieldFactory);
+        }
+
+
+        public abstract void addValidator();
+
+        public abstract void addListener();
+
+        /**
+         * @return the fieldGroup
+         */
+        public BeanFieldGroup<T> getFieldGroup() {
+            return fieldGroup;
+        }
+
+        /**
+         * @param fieldGroup
+         */
+        public void setFieldGroup(BeanFieldGroup<T> fieldGroup) {
+            this.fieldGroup = fieldGroup;
+        }
+
+        protected abstract B self();
+
+    }
 
 
 }
