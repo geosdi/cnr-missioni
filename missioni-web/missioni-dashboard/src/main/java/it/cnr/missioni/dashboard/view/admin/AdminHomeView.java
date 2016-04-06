794c371d326bb0c1b2675fe64171d45bdffa766b
vito.salvia@gmail.com <vito.salvia@gmail.com>
2016-04-06 11:17:32 +0200

    refactoring

--- missioni-web/missioni-dashboard/src/main/java/it/cnr/missioni/dashboard/view/admin/AdminHomeView.java
+++ missioni-web/missioni-dashboard/src/main/java/it/cnr/missioni/dashboard/view/admin/AdminHomeView.java
@@ -3,7 +3,6 @@ package it.cnr.missioni.dashboard.view.admin;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.Notification.Type;
-
 import it.cnr.missioni.dashboard.client.ClientConnector;
 import it.cnr.missioni.dashboard.component.table.admin.ElencoStatisticheMissioniTable;
 import it.cnr.missioni.dashboard.utility.Utility;
@@ -14,48 +13,43 @@ import it.cnr.missioni.el.model.search.builder.SearchConstants;
 
 public final class AdminHomeView extends HomeView {
 
-	protected IMissioneSearchBuilder missioneSearchBuilder = IMissioneSearchBuilder.MissioneSearchBuilder.getMissioneSearchBuilder();
-	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 6899889980687530074L;
+    /**
+     *
+     */
+    private static final long serialVersionUID = 6899889980687530074L;
+    protected IMissioneSearchBuilder missioneSearchBuilder = IMissioneSearchBuilder.MissioneSearchBuilder.getMissioneSearchBuilder();
 
-	public AdminHomeView() {
-		super();
+    public AdminHomeView() {
+        super();
+    }
 
-	}
-	
-	@Override
-	public void enter(final ViewChangeEvent event) {
-		try {
-			elencoMissioniTable.aggiornaTableAdmin(ClientConnector.getMissione(IMissioneSearchBuilder.MissioneSearchBuilder.getMissioneSearchBuilder()));
+    @Override
+    public void enter(final ViewChangeEvent event) {
+        try {
+            elencoMissioniTable.aggiornaTableAdmin(ClientConnector.getMissione(IMissioneSearchBuilder.MissioneSearchBuilder.getMissioneSearchBuilder()));
 
-			elencoRimborsiTable.aggiornaTableAdmin(ClientConnector.getMissione(IMissioneSearchBuilder.MissioneSearchBuilder.getMissioneSearchBuilder()
-					.withFieldExist(SearchConstants.MISSIONE_FIELD_RIMBORSO)));
+            elencoRimborsiTable.aggiornaTableAdmin(ClientConnector.getMissione(IMissioneSearchBuilder.MissioneSearchBuilder.getMissioneSearchBuilder()
+                    .withFieldExist(SearchConstants.MISSIONE_FIELD_RIMBORSO)));
+        } catch (Exception e) {
+            Utility.getNotification(Utility.getMessage("error_message"), Utility.getMessage("request_error"),
+                    Type.ERROR_MESSAGE);
+        }
+    }
 
-		} catch (Exception e) {
-			Utility.getNotification(Utility.getMessage("error_message"), Utility.getMessage("request_error"),
-					Type.ERROR_MESSAGE);
-		}
-
-	}
-	
-	@Override
-	protected Component buildPanelBottom() {
-		ElencoStatisticheMissioniTable elencoStatisticheMissioniTable = new ElencoStatisticheMissioniTable();
-		elencoStatisticheMissioniTable.setCaption("Statistiche Missioni");
-		try {
-			StatisticheMissioni statisticheMissioni = ClientConnector.getStatisticheMissioni();
-			elencoStatisticheMissioniTable.aggiornaTable(statisticheMissioni);
-		} catch (Exception e) {
-			Utility.getNotification(Utility.getMessage("error_message"), Utility.getMessage("request_error"),
-					Type.ERROR_MESSAGE);
-		}
-		
-		Component panel = createContentWrapper(elencoStatisticheMissioniTable);
-		panel.setId("statistiche");
-		return panel;
-	}
+    @Override
+    protected Component buildPanelBottom() {
+        ElencoStatisticheMissioniTable elencoStatisticheMissioniTable = new ElencoStatisticheMissioniTable();
+        elencoStatisticheMissioniTable.setCaption("Statistiche Missioni");
+        try {
+            StatisticheMissioni statisticheMissioni = ClientConnector.getStatisticheMissioni();
+            elencoStatisticheMissioniTable.aggiornaTable(statisticheMissioni);
+        } catch (Exception e) {
+            Utility.getNotification(Utility.getMessage("error_message"), Utility.getMessage("request_error"),
+                    Type.ERROR_MESSAGE);
+        }
+        Component panel = createContentWrapper(elencoStatisticheMissioniTable);
+        panel.setId("statistiche");
+        return panel;
+    }
 
 }
