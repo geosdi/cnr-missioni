bc986edb1e17da61572952004de57e8c96e7933e
vito.salvia@gmail.com <vito.salvia@gmail.com>
2016-04-06 11:23:19 +0200

    refactoring

--- missioni-web/missioni-dashboard/src/main/java/it/cnr/missioni/dashboard/utility/AdvancedFileDownloader.java
+++ missioni-web/missioni-dashboard/src/main/java/it/cnr/missioni/dashboard/utility/AdvancedFileDownloader.java
@@ -1,31 +1,22 @@
 package it.cnr.missioni.dashboard.utility;
 
+import com.vaadin.server.*;
+import com.vaadin.ui.AbstractComponent;
+
 import java.io.IOException;
 import java.util.logging.Logger;
 
-import com.vaadin.server.ConnectorResource;
-import com.vaadin.server.DownloadStream;
-import com.vaadin.server.FileDownloader;
-import com.vaadin.server.Resource;
-import com.vaadin.server.StreamResource;
-import com.vaadin.server.VaadinRequest;
-import com.vaadin.server.VaadinResponse;
-import com.vaadin.server.VaadinSession;
-import com.vaadin.ui.AbstractComponent;
-
 /**
- * 
  * @author Salvia Vito
- *
  */
 public class AdvancedFileDownloader extends FileDownloader {
 
     /**
-	 * 
-	 */
-	private static final long serialVersionUID = -1377598357403435904L;
+     *
+     */
+    private static final long serialVersionUID = -1377598357403435904L;
 
-	/**
+    /**
      *
      */
     private static final boolean DEBUG_MODE = true;
@@ -37,27 +28,15 @@ public class AdvancedFileDownloader extends FileDownloader {
 
     private AdvancedDownloaderListener dynamicDownloaderListener;
     private DownloaderEvent downloadEvent;
+    private StreamResource resource;
 
-    public abstract class DownloaderEvent {
-
-        /**
-         * 
-         * @return
-         */
-        public abstract AbstractComponent getExtendedComponet();
-
-        public abstract void setExtendedComponet(
-                AbstractComponent extendedComponet);
-
+    private AdvancedFileDownloader(StreamResource resource) {
+        super(resource == null ? (resource = new StreamResource(null, "")) : resource);
+        AdvancedFileDownloader.this.resource = resource;
     }
 
-    public interface AdvancedDownloaderListener {
-        /**
-         * Invocato prima che parta il download
-         * 
-         * @param downloadEvent
-         */
-        public void beforeDownload(DownloaderEvent downloadEvent);
+    public AdvancedFileDownloader() {
+        this(null);
     }
 
     public void fireEvent() {
@@ -77,19 +56,18 @@ public class AdvancedFileDownloader extends FileDownloader {
             AdvancedDownloaderListener listener) {
         if (listener != null) {
             DownloaderEvent downloadEvent = new DownloaderEvent() {
-
                 private AbstractComponent extendedComponet;
-
-                @Override
-                public void setExtendedComponet(
-                        AbstractComponent extendedComponet) {
-                    this.extendedComponet = extendedComponet;
-                }
 
                 @Override
                 public AbstractComponent getExtendedComponet() {
                     // TODO Auto-generated method stub
                     return this.extendedComponet;
+                }
+
+                @Override
+                public void setExtendedComponet(
+                        AbstractComponent extendedComponet) {
+                    this.extendedComponet = extendedComponet;
                 }
             };
             downloadEvent
@@ -100,61 +78,59 @@ public class AdvancedFileDownloader extends FileDownloader {
         }
     }
 
-
-    private StreamResource resource;
-
-    private AdvancedFileDownloader(StreamResource resource) {
-
-        super(resource == null ? (resource = new StreamResource(null,"")) : resource);
-
-        AdvancedFileDownloader.this.resource = resource;
-    }
-
-    public AdvancedFileDownloader() {
-        this(null);
-    }
-
-
     @Override
     public boolean handleConnectorRequest(VaadinRequest request,
             VaadinResponse response, String path) throws IOException {
 
-       
         if (!path.matches("dl(/.*)?")) {
             // Ignore if it isn't for us
             return false;
         }
         VaadinSession session = getSession();
-
         session.lock();
         AdvancedFileDownloader.this.fireEvent();
-
         DownloadStream stream;
-
         try {
             Resource resource = getFileDownloadResource();
             if (!(resource instanceof ConnectorResource)) {
                 return false;
             }
             stream = ((ConnectorResource) resource).getStream();
-
             if (stream.getParameter("Content-Disposition") == null) {
                 // Content-Disposition: attachment generally forces download
                 stream.setParameter("Content-Disposition",
                         "attachment; filename=\"" + stream.getFileName() + "\"");
             }
-
             // Content-Type to block eager browser plug-ins from hijacking
             // the file
             if (isOverrideContentType()) {
                 stream.setContentType("application/octet-stream;charset=UTF-8");
             }
-           
         } finally {
             session.unlock();
         }
         stream.writeResponse(request, response);
         return true;
-    } 
+    }
+
+    public interface AdvancedDownloaderListener {
+        /**
+         * Invocato prima che parta il download
+         *
+         * @param downloadEvent
+         */
+        public void beforeDownload(DownloaderEvent downloadEvent);
+    }
+
+    public abstract class DownloaderEvent {
+
+        /**
+         * @return
+         */
+        public abstract AbstractComponent getExtendedComponet();
+
+        public abstract void setExtendedComponet(
+                AbstractComponent extendedComponet);
+    }
 }
 
