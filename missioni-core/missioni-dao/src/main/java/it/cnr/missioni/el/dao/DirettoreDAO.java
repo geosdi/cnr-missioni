215b33bea482fdfbbfc84a68690d7f05a0aded73
Vito Salvia <vito.salvia@gmail.com>
2016-04-27 17:26:14 +0200

    Refactoring

--- missioni-core/missioni-dao/src/main/java/it/cnr/missioni/el/dao/DirettoreDAO.java
+++ missioni-core/missioni-dao/src/main/java/it/cnr/missioni/el/dao/DirettoreDAO.java
@@ -1,5 +1,6 @@
 package it.cnr.missioni.el.dao;
 
+import com.google.common.base.Preconditions;
 import it.cnr.missioni.model.configuration.Direttore;
 import org.geosdi.geoplatform.experimental.el.api.mapper.GPBaseMapper;
 import org.geosdi.geoplatform.experimental.el.dao.AbstractElasticSearchDAO;
@@ -25,6 +26,13 @@ public class DirettoreDAO extends AbstractElasticSearchDAO<Direttore> implements
     @Override
     public <IC extends GPIndexCreator> void setIndexCreator(IC ic) {
         super.setIndexCreator(ic);
+    }
+
+    @Override
+    public Direttore persist(Direttore document) throws Exception {
+        logger.debug("#################Try to insert {}\n\n", document);
+        Preconditions.checkArgument((count().intValue() == 0), "Direttore Id already inserted");
+        return super.persist(document);
     }
 
     /**
