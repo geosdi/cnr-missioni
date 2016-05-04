654d4ebc4449e6be83b75866f468ff749195e17f
Vito Salvia <vito.salvia@gmail.com>
2016-05-04 10:34:57 +0200

    Added data backup test

---
+++ missioni-core/missioni-dao/src/test/java/it/cnr/missioni/el/restore/IBackupStore.java
@@ -0,0 +1,173 @@
+package it.cnr.missioni.el.restore;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import it.cnr.missioni.model.configuration.Direttore;
+import it.cnr.missioni.model.configuration.Massimale;
+import it.cnr.missioni.model.configuration.Nazione;
+import it.cnr.missioni.model.configuration.QualificaUser;
+import it.cnr.missioni.model.configuration.RimborsoKm;
+import it.cnr.missioni.model.configuration.TipologiaSpesa;
+import it.cnr.missioni.model.configuration.UrlImage;
+import it.cnr.missioni.model.missione.Missione;
+import it.cnr.missioni.model.prenotazione.Prenotazione;
+import it.cnr.missioni.model.prenotazione.VeicoloCNR;
+import it.cnr.missioni.model.user.User;
+
+public interface IBackupStore<E> {
+	
+	void setList(List<E> list);
+	
+	List<E> getList();
+	
+	public class NazioneBackupStore implements IBackupStore<Nazione>{
+
+		private List<Nazione> list = new ArrayList();
+
+		public List<Nazione> getList() {
+			return list;
+		}
+
+		public void setList(List<Nazione> list) {
+			this.list = list;
+		}
+		
+	}
+	
+	public class TipologiaSpesaBackupStore implements IBackupStore<TipologiaSpesa>{
+
+		private List<TipologiaSpesa> list = new ArrayList();
+
+		public List<TipologiaSpesa> getList() {
+			return list;
+		}
+
+		public void setList(List<TipologiaSpesa> list) {
+			this.list = list;
+		}
+	}
+	
+	public class MassimaleBackupStore implements IBackupStore<Massimale>{
+
+		private List<Massimale> list = new ArrayList();
+
+		public List<Massimale> getList() {
+			return list;
+		}
+
+		public void setList(List<Massimale> list) {
+			this.list = list;
+		}
+	}
+	
+	public class DirettoreBackupStore implements IBackupStore<Direttore>{
+
+		private List<Direttore> list = new ArrayList();
+
+		public List<Direttore> getList() {
+			return list;
+		}
+
+		public void setList(List<Direttore> list) {
+			this.list = list;
+		}
+	}
+	
+	public class UrlImageBackupStore implements IBackupStore<UrlImage>{
+
+		private List<UrlImage> list = new ArrayList();
+
+		public List<UrlImage> getList() {
+			return list;
+		}
+
+		public void setList(List<UrlImage> list) {
+			this.list = list;
+		}
+	}
+	
+	public class QualificaBackupStore implements IBackupStore<QualificaUser>{
+
+		private List<QualificaUser> list = new ArrayList();
+
+		public List<QualificaUser> getList() {
+			return list;
+		}
+
+		public void setList(List<QualificaUser> list) {
+			this.list = list;
+		}
+	}
+	
+	public class RimborsoKmBackupStore implements IBackupStore<RimborsoKm>{
+
+		private List<RimborsoKm> list = new ArrayList();
+
+		public List<RimborsoKm> getList() {
+			return list;
+		}
+
+		public void setList(List<RimborsoKm> list) {
+			this.list = list;
+		}
+	}
+	
+	public class UserBackupStore implements IBackupStore<User>{
+
+		private List<User> list = new ArrayList();
+
+		public List<User> getList() {
+			return list;
+		}
+
+		public void setList(List<User> list) {
+			this.list = list;
+		}
+		
+	}
+	
+	public class MissioneBackupStore implements IBackupStore<Missione>{
+
+		private List<Missione> list = new ArrayList();
+
+		public List<Missione> getList() {
+			return list;
+		}
+
+		public void setList(List<Missione> list) {
+			this.list = list;
+		}
+		
+	}
+	
+	public class PrenotazioneBackupStore implements IBackupStore<Prenotazione>{
+
+		private List<Prenotazione> list = new ArrayList();
+
+		public List<Prenotazione> getList() {
+			return list;
+		}
+
+		public void setList(List<Prenotazione> list) {
+			this.list = list;
+		}
+		
+	}
+	public class VeicoloCnrBackupStore implements IBackupStore<VeicoloCNR>{
+
+		private List<VeicoloCNR> list = new ArrayList();
+
+		public List<VeicoloCNR> getList() {
+			return list;
+		}
+
+		public void setList(List<VeicoloCNR> list) {
+			this.list = list;
+		}
+		
+	}
+	
+}
+
+
--- missioni-core/missioni-dao/src/test/java/it/cnr/missioni/el/restore/RestoreDAOTest.java
+++ missioni-core/missioni-dao/src/test/java/it/cnr/missioni/el/restore/RestoreDAOTest.java
@@ -1,11 +1,7 @@
 package it.cnr.missioni.el.restore;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.UUID;
 
 import javax.annotation.Resource;
 
@@ -13,11 +9,8 @@ import org.geosdi.geoplatform.experimental.el.configurator.GPIndexConfigurator;
 import org.geosdi.geoplatform.experimental.el.dao.GPPageableElasticSearchDAO.Page;
 import org.geosdi.geoplatform.experimental.el.index.GPIndexCreator;
 import org.geosdi.geoplatform.logger.support.annotation.GeoPlatformLog;
-import org.joda.time.DateTime;
-import org.joda.time.chrono.ISOChronology;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -26,33 +19,42 @@ import org.slf4j.Logger;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import it.cnr.missioni.el.dao.IDirettoreDAO;
 import it.cnr.missioni.el.dao.IMassimaleDAO;
 import it.cnr.missioni.el.dao.IMissioneDAO;
 import it.cnr.missioni.el.dao.INazioneDAO;
 import it.cnr.missioni.el.dao.IPrenotazioneDAO;
 import it.cnr.missioni.el.dao.IQualificaUserDAO;
+import it.cnr.missioni.el.dao.IRimborsoKmDAO;
 import it.cnr.missioni.el.dao.ITipologiaSpesaDAO;
+import it.cnr.missioni.el.dao.IUrlImageDAO;
 import it.cnr.missioni.el.dao.IUserDAO;
 import it.cnr.missioni.el.dao.IVeicoloCNRDAO;
-import it.cnr.missioni.el.mapper.NazioneMapper;
+import it.cnr.missioni.el.restore.IBackupStore.DirettoreBackupStore;
+import it.cnr.missioni.el.restore.IBackupStore.MassimaleBackupStore;
+import it.cnr.missioni.el.restore.IBackupStore.MissioneBackupStore;
+import it.cnr.missioni.el.restore.IBackupStore.NazioneBackupStore;
+import it.cnr.missioni.el.restore.IBackupStore.PrenotazioneBackupStore;
+import it.cnr.missioni.el.restore.IBackupStore.QualificaBackupStore;
+import it.cnr.missioni.el.restore.IBackupStore.RimborsoKmBackupStore;
+import it.cnr.missioni.el.restore.IBackupStore.TipologiaSpesaBackupStore;
+import it.cnr.missioni.el.restore.IBackupStore.UrlImageBackupStore;
+import it.cnr.missioni.el.restore.IBackupStore.UserBackupStore;
+import it.cnr.missioni.el.restore.IBackupStore.VeicoloCnrBackupStore;
+import it.cnr.missioni.model.configuration.Direttore;
 import it.cnr.missioni.model.configuration.Massimale;
 import it.cnr.missioni.model.configuration.Nazione;
-import it.cnr.missioni.model.configuration.Nazione.AreaGeograficaEnum;
 import it.cnr.missioni.model.configuration.QualificaUser;
+import it.cnr.missioni.model.configuration.RimborsoKm;
 import it.cnr.missioni.model.configuration.TipologiaSpesa;
-import it.cnr.missioni.model.configuration.TipologiaSpesa.VoceSpesaEnum;
-import it.cnr.missioni.model.missione.TrattamentoMissioneEsteraEnum;
-import it.cnr.missioni.model.user.Anagrafica;
-import it.cnr.missioni.model.user.Anagrafica.Genere;
-import it.cnr.missioni.model.user.Credenziali;
-import it.cnr.missioni.model.user.DatiCNR;
-import it.cnr.missioni.model.user.DatiCNR.LivelloUserEnum;
-import it.cnr.missioni.model.user.Patente;
-import it.cnr.missioni.model.user.Residenza;
-import it.cnr.missioni.model.user.RuoloUserEnum;
+import it.cnr.missioni.model.configuration.UrlImage;
+import it.cnr.missioni.model.missione.Missione;
+import it.cnr.missioni.model.prenotazione.Prenotazione;
+import it.cnr.missioni.model.prenotazione.VeicoloCNR;
 import it.cnr.missioni.model.user.User;
-import it.cnr.missioni.model.user.Veicolo;
-import it.cnr.missioni.support.builder.generator.IMd5PasswordGenerator;
 
 /**
  * @author Salvia Vito
@@ -91,6 +93,21 @@ public class RestoreDAOTest {
     private GPIndexCreator userDocIndexCreator;
     @Resource(name = "userDAO")
     private IUserDAO userDAO;
+    
+    @Resource(name = "direttoreIndexCreator")
+    private GPIndexCreator direttoreDocIndexCreator;
+    @Resource(name = "direttoreDAO")
+    private IDirettoreDAO direttoreDAO;
+    
+    @Resource(name = "rimborsoKmIndexCreator")
+    private GPIndexCreator rimborsoKmDocIndexCreator;
+    @Resource(name = "rimborsoKmDAO")
+    private IRimborsoKmDAO rimborsoKmDAO;
+    
+    @Resource(name = "urlImageIndexCreator")
+    private GPIndexCreator urlImageDocIndexCreator;
+    @Resource(name = "urlImageDAO")
+    private IUrlImageDAO urlImageDAO;
 
     @Resource(name = "nazioneIndexCreator")
     private GPIndexCreator nazioneDocIndexCreator;
@@ -106,10 +123,9 @@ public class RestoreDAOTest {
     private GPIndexCreator massimaleDocIndexCreator;
     @Resource(name = "massimaleDAO")
     private IMassimaleDAO massimaleDAO;
+
     
-    private final NazioneMapper nazioneMapper = new NazioneMapper();
-
-
+    ObjectMapper mapper = new ObjectMapper();
 
     @Before
     public void setUp() {
@@ -133,10 +149,190 @@ public class RestoreDAOTest {
     }
     
     @Test
-    public void restoreNazioneTest() throws Exception {
+    public void a_backupNazioneTest() throws Exception {
     	List<Nazione> lista = this.nazioneDAO.find(new Page(0,this.nazioneDAO.count().intValue())).getResults();
-    	NazioneConf conf = new NazioneConf();
+    	IBackupStore<Nazione> conf = new NazioneBackupStore();
     	conf.setList(lista);
+    	mapper.enable(SerializationFeature.INDENT_OUTPUT);
+    	mapper.writeValue(new File("./target/NazioneBackup.json"), conf);
+    }
+    
+    @Test
+    public void b_backupTipologiaSpesaTest() throws Exception {
+    	List<TipologiaSpesa> lista = this.tipologiaSpesaDAO.find(new Page(0,this.tipologiaSpesaDAO.count().intValue())).getResults();
+    	IBackupStore<TipologiaSpesa> conf = new TipologiaSpesaBackupStore();
+    	conf.setList(lista);
+    	mapper.enable(SerializationFeature.INDENT_OUTPUT);
+    	mapper.writeValue(new File("./target/TipologiaSpesaBackup.json"), conf);
+    }
+    
+    @Test
+    public void c_backupMassimaleTest() throws Exception {
+    	List<Massimale> lista = this.massimaleDAO.find(new Page(0,this.massimaleDAO.count().intValue())).getResults();
+    	IBackupStore<Massimale> conf = new MassimaleBackupStore();
+    	conf.setList(lista);
+    	mapper.enable(SerializationFeature.INDENT_OUTPUT);
+    	mapper.writeValue(new File("./target/MassimaleBackup.json"), conf);
+    }
+    
+    @Test
+    public void d_backupDirettoreTest() throws Exception {
+    	List<Direttore> lista = this.direttoreDAO.find(new Page(0,this.direttoreDAO.count().intValue())).getResults();
+    	IBackupStore<Direttore> conf = new DirettoreBackupStore();
+    	conf.setList(lista);
+    	mapper.enable(SerializationFeature.INDENT_OUTPUT);
+    	mapper.writeValue(new File("./target/DirettoreBackup.json"), conf);
+    }
+    
+    @Test
+    public void e_backupUrlImageTest() throws Exception {
+    	List<UrlImage> lista = this.urlImageDAO.find(new Page(0,this.urlImageDAO.count().intValue())).getResults();
+    	IBackupStore<UrlImage> conf = new UrlImageBackupStore();
+    	conf.setList(lista);
+    	mapper.enable(SerializationFeature.INDENT_OUTPUT);
+    	mapper.writeValue(new File("./target/UrlImageBackup.json"), conf);
+    }
+    
+    @Test
+    public void f_backupQualificaTest() throws Exception {
+    	List<QualificaUser> lista = this.qualificaUserDAO.find(new Page(0,this.qualificaUserDAO.count().intValue())).getResults();
+    	IBackupStore<QualificaUser> conf = new QualificaBackupStore();
+    	conf.setList(lista);
+    	mapper.enable(SerializationFeature.INDENT_OUTPUT);
+    	mapper.writeValue(new File("./target/QualificaUserBackup.json"), conf);
+    }
+    
+    @Test
+    public void g_backupRimborsoKmTest() throws Exception {
+    	List<RimborsoKm> lista = this.rimborsoKmDAO.find(new Page(0,this.rimborsoKmDAO.count().intValue())).getResults();
+    	IBackupStore<RimborsoKm> conf = new RimborsoKmBackupStore();
+    	conf.setList(lista);
+    	mapper.enable(SerializationFeature.INDENT_OUTPUT);
+    	mapper.writeValue(new File("./target/RimborsoKmBackup.json"), conf);
+    }
+    
+    @Test
+    public void h_backupUserTest() throws Exception {
+    	List<User> lista = this.userDAO.find(new Page(0,this.userDAO.count().intValue())).getResults();
+    	IBackupStore<User> conf = new IBackupStore.UserBackupStore();
+    	conf.setList(lista);
+    	mapper.enable(SerializationFeature.INDENT_OUTPUT);
+    	mapper.writeValue(new File("./target/UserBackup.json"), conf);
+    }
+    
+    @Test
+    public void i_backupMissioneTest() throws Exception {
+    	List<Missione> lista = this.missioneDAO.find(new Page(0,this.missioneDAO.count().intValue())).getResults();
+    	IBackupStore<Missione> conf = new IBackupStore.MissioneBackupStore();
+    	conf.setList(lista);
+    	mapper.enable(SerializationFeature.INDENT_OUTPUT);
+    	mapper.writeValue(new File("./target/MissioneBackup.json"), conf);
+    }
+    
+    @Test
+    public void l_backupPrenotazioneTest() throws Exception {
+    	List<Prenotazione> lista = this.prenotazioneDAO.find(new Page(0,this.prenotazioneDAO.count().intValue())).getResults();
+    	IBackupStore<Prenotazione> conf = new IBackupStore.PrenotazioneBackupStore();
+    	conf.setList(lista);
+    	mapper.enable(SerializationFeature.INDENT_OUTPUT);
+    	mapper.writeValue(new File("./target/PrenotazioneBackup.json"), conf);
+    }
+    
+    @Test
+    public void m_backupVeicoloCnrTest() throws Exception {
+    	List<VeicoloCNR> lista = this.veicoloCNRDAO.find(new Page(0,this.veicoloCNRDAO.count().intValue())).getResults();
+    	IBackupStore<VeicoloCNR> conf = new IBackupStore.VeicoloCnrBackupStore();
+    	conf.setList(lista);
+    	mapper.enable(SerializationFeature.INDENT_OUTPUT);
+    	mapper.writeValue(new File("./target/VeicoloCnrBackup.json"), conf);
+    }
+    
+    @Test
+    public void n_readNazioneBackupTest() throws Exception {
+    	IBackupStore<Nazione> conf = mapper.readValue(new File("./target/NazioneBackup.json"), NazioneBackupStore.class);
+    	conf.getList().stream().forEach(f->{
+    		logger.info("@@@@@@@@@@@@@@NAZIONE: {}\n",f);
+    	});
+    }
+    
+    @Test
+    public void o_readTipologiaSpesaBackupTest() throws Exception {
+    	IBackupStore<TipologiaSpesa> conf = mapper.readValue(new File("./target/TipologiaSpesaBackup.json"), TipologiaSpesaBackupStore.class);
+    	conf.getList().stream().forEach(f->{
+    		logger.info("@@@@@@@@@@@@@@TIPOLOGIA SPESA: {}\n",f);
+    	});
+    }
+    
+    @Test
+    public void p_readMassimaleBackupTest() throws Exception {
+    	IBackupStore<Massimale> conf = mapper.readValue(new File("./target/MassimaleBackup.json"), MassimaleBackupStore.class);
+    	conf.getList().stream().forEach(f->{
+    		logger.info("@@@@@@@@@@@@@@MASSIMALE: {}\n",f);
+    	});
+    }
+    
+    @Test
+    public void q_readDirettoreBackupTest() throws Exception {
+    	IBackupStore<Direttore> conf = mapper.readValue(new File("./target/DirettoreBackup.json"), DirettoreBackupStore.class);
+    	conf.getList().stream().forEach(f->{
+    		logger.info("@@@@@@@@@@@@@@DIRETTORE: {}\n",f);
+    	});
+    }
+    
+    @Test
+    public void r_readUrlImageBackupTest() throws Exception {
+    	IBackupStore<UrlImage> conf = mapper.readValue(new File("./target/UrlImageBackup.json"), UrlImageBackupStore.class);
+    	conf.getList().stream().forEach(f->{
+    		logger.info("@@@@@@@@@@@@@@URL IMAGE: {}\n",f);
+    	});
+    }
+    
+    @Test
+    public void s_readQualificaBackupTest() throws Exception {
+    	IBackupStore<QualificaUser> conf = mapper.readValue(new File("./target/QualificaUserBackup.json"), QualificaBackupStore.class);
+    	conf.getList().stream().forEach(f->{
+    		logger.info("@@@@@@@@@@@@@@QUALIFICA: {}\n",f);
+    	});
+    }
+    
+    @Test
+    public void t_readRimborsoKmBackupTest() throws Exception {
+    	IBackupStore<RimborsoKm> conf = mapper.readValue(new File("./target/RimborsoKmBackup.json"), RimborsoKmBackupStore.class);
+    	conf.getList().stream().forEach(f->{
+    		logger.info("@@@@@@@@@@@@@@RIMBORSO KM: {}\n",f);
+    	});
+    }
+    
+    @Test
+    public void u_readUserBackupTest() throws Exception {
+    	IBackupStore<User> conf = mapper.readValue(new File("./target/UserBackup.json"), UserBackupStore.class);
+    	conf.getList().stream().forEach(f->{
+    		logger.info("@@@@@@@@@@@@@@USER: {}\n",f);
+    	});
+    }
+    
+    @Test
+    public void v_readMissioneBackupTest() throws Exception {
+    	IBackupStore<Missione> conf = mapper.readValue(new File("./target/MissioneBackup.json"), MissioneBackupStore.class);
+    	conf.getList().stream().forEach(f->{
+    		logger.info("@@@@@@@@@@@@@@MISSIONE: {}\n",f);
+    	});
+    }
+    
+    @Test
+    public void x_readPrenotazioneBackupTest() throws Exception {
+    	IBackupStore<Prenotazione> conf = mapper.readValue(new File("./target/PrenotazioneBackup.json"), PrenotazioneBackupStore.class);
+    	conf.getList().stream().forEach(f->{
+    		logger.info("@@@@@@@@@@@@@@PRENOTAZIONE: {}\n",f);
+    	});
+    }
+    
+    @Test
+    public void y_readVeicoloCnrBackupTest() throws Exception {
+    	IBackupStore<VeicoloCNR> conf = mapper.readValue(new File("./target/VeicoloCnrBackup.json"), VeicoloCnrBackupStore.class);
+    	conf.getList().stream().forEach(f->{
+    		logger.info("@@@@@@@@@@@@@@VEICOLO CNR: {}\n",f);
+    	});
     }
 
 }
