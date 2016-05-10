fdbcdb7d98b18db9398b30f88a9626e9072f0850
Vito Salvia <vito.salvia@gmail.com>
2016-05-10 12:36:31 +0200

    Refactoring

--- missioni-web/missioni-dashboard/src/main/java/it/cnr/missioni/dashboard/broadcast/Broadcaster.java
+++ missioni-web/missioni-dashboard/src/main/java/it/cnr/missioni/dashboard/broadcast/Broadcaster.java
@@ -11,25 +11,25 @@ public class Broadcaster implements Serializable {
     /**
      *
      */
-    private static final long serialVersionUID = 4894282195273039938L;
-
-    private static final List<BroadcastListener> listeners = new CopyOnWriteArrayList<BroadcastListener>();
-
-    public static void register(BroadcastListener listener) {
-        listeners.add(listener);
-    }
-
-    public static void unregister(BroadcastListener listener) {
-        listeners.remove(listener);
-    }
-
-    public static void broadcast(final String message) {
-        for (BroadcastListener listener : listeners) {
-            listener.receiveBroadcast(message);
-        }
-    }
-
-    public interface BroadcastListener {
-        public void receiveBroadcast(String message);
-    }
+//    private static final long serialVersionUID = 4894282195273039938L;
+//
+//    private static final List<BroadcastListener> listeners = new CopyOnWriteArrayList<BroadcastListener>();
+//
+//    public static void register(BroadcastListener listener) {
+//        listeners.add(listener);
+//    }
+//
+//    public static void unregister(BroadcastListener listener) {
+//        listeners.remove(listener);
+//    }
+//
+//    public static void broadcast(final String message) {
+//        for (BroadcastListener listener : listeners) {
+//            listener.receiveBroadcast(message);
+//        }
+//    }
+//
+//    public interface BroadcastListener {
+//        public void receiveBroadcast(String message);
+//    }
 }
